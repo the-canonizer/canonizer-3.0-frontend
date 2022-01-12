ccee9bd2c4904b604cd1d6eb3446bcec1591be84
adeelahmed-codedistrict <adeel.ahmed@codedistrict.com>
2022-01-12 18:52:56 +0500

    -- key attribute added to header menu item --

--- src/components/common/headers/loggedOutHeader/index.tsx
+++ src/components/common/headers/loggedOutHeader/index.tsx
@@ -18,25 +18,25 @@ const LoggedOutHeader = () => {
         </div>
         <nav className={styles.navWrap}>
           <Menu mode="horizontal" className={styles.nav}>
-            <Menu.Item>
+            <Menu.Item key={0}>
               <a href=""> Browse </a>
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item key={1}>
               <a href="">Upload File</a>
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item key={2}>
               <a href="">Help</a>
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item key={3}>
               <a href="">White Paper</a>
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item key={4}>
               <a href="">Blog</a>
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item key={5}>
               <a href="">Jobs</a>
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item key={6}>
               <a href="">Services</a>
             </Menu.Item>
           </Menu>
