7840ebedc418fec39f7fdc1bbf559e177b3ca96f
Vikas Rana <vikas.rana@talentelgia.in>
2022-02-01 19:12:48 +0530

    CAN-68: Manage Profile: Frontend API integration : Verify Mobile Number
    Implemented API to Fetch the list of mobile carriers
    Implemented API to send OTP for phone number verification
    Implemented API to verify OTP

--- src/components/ComponentPages/ProfileInfo/index.tsx
+++ src/components/ComponentPages/ProfileInfo/index.tsx
@@ -3,13 +3,21 @@ import { useDispatch } from "react-redux";
 import moment from 'moment';
 import { Form, message } from "antd";
 import { AppDispatch } from "../../../store";
-import { GetUserProfileInfo, UpdateUserProfileInfo } from "../../../network/services/auth/index";
+import { GetUserProfileInfo, UpdateUserProfileInfo, GetMobileCarrier, SendOTP, VerifyOTP } from "../../../network/services/auth/index";
 import ProfileInfoUI from "./ProfileInfoUI";
 const ProfileInfo = () => {
   const dispatch = useDispatch<AppDispatch>();
   const [form] = Form.useForm();
+  const [formVerify] = Form.useForm();
+  const [mobileCarrier, setMobileCarrier] = useState([]);
+  const [isOTPModalVisible, setIsOTPModalVisible] = useState(false);
+  const [oTP, setOTP] = useState();
 
+
+  //on update profile click
   const onFinish = async (values: any) => {
+    values.mobile_carrier = formVerify.getFieldValue("mobile_carrier")
+    values.phone_number = formVerify.getFieldValue("phone_number")
     let res = await dispatch(UpdateUserProfileInfo(values));
     if (res && res.status_code === 200) {
       message.success(res.message);
@@ -18,22 +26,74 @@ const ProfileInfo = () => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
-
+  //Send OTP to mobile number
+  const onVerifyClick = async (values: any) => {
+    let res = await dispatch(SendOTP(values));
+    if (res && res.status_code === 200) {
+      message.success(res.message);
+      setIsOTPModalVisible(true);
+      console.log(res.data.otp);
+      //setOTP(res.data.otp)
+    }
+  };
+  const handleOTPCancel = () => {
+    setIsOTPModalVisible(false);
+  };
+  //function to verify the OTP
+  const onOTPBtnClick = async () => {
+    let otpBody = {
+      otp: oTP
+    };
+    let res = await dispatch(VerifyOTP(otpBody));
+    if (res && res.status_code === 200) {
+      message.success(res.message);
+      setIsOTPModalVisible(false);
+    }
+    else {
+      message.error(res.message);
+    }
+  };
+  const handleChangeOTP = (e) => {
+    setOTP(e.target.value);
+  }
   useEffect(() => {
+    async function fetchMobileCarrier() {
+      let res = await dispatch(GetMobileCarrier());
+      if (res != undefined) {
+        setMobileCarrier(res.data);
+      }
+    }
     async function fetchUserProfileInfo() {
       let res = await dispatch(GetUserProfileInfo());
-      //format date for datepicker
-      res.data.birthday=moment(res.data.birthday, 'YYYY-MM-DD');
-      form.setFieldsValue(res.data);
-      
+      if (res != undefined) {
+        if (res.data != undefined) {
+          const verify = {
+            phone_number: res.data.phone_number,
+            mobile_carrier: res.data.mobile_carrier
+          }
+          formVerify.setFieldsValue(verify);
+          //format date for datepicker
+          res.data.birthday = moment(res.data.birthday, 'YYYY-MM-DD');
+          form.setFieldsValue(res.data);
+        }
+      }
     }
+    fetchMobileCarrier()
     fetchUserProfileInfo()
   }, []);
   return (
     <ProfileInfoUI
-    form={form}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
+      form={form}
+      formVerify={formVerify}
+      mobileCarrier={mobileCarrier}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      onVerifyClick={onVerifyClick}
+      onOTPBtnClick={onOTPBtnClick}
+      isOTPModalVisible={isOTPModalVisible}
+      handleOTPCancel={handleOTPCancel}
+      oTP={oTP}
+      handleChangeOTP={handleChangeOTP}
     ></ProfileInfoUI>
   );
 };
