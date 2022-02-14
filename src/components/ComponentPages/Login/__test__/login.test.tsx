1de4d3bba46415da0d33d11d5fe51e66ac14e8b9
Rahul Kumar Singh <rahul.singh@iffort.com>
2022-02-14 10:55:05 +0530

    test cases for login registration and forgot password

--- src/components/ComponentPages/Login/__test__/login.test.tsx
+++ src/components/ComponentPages/Login/__test__/login.test.tsx
@@ -1,73 +1,132 @@
-import { render, screen, waitFor } from "../src/utils/testUtils";
+import { render, screen, waitFor } from "../../../../utils/testUtils";
 import userEvent from "@testing-library/user-event";
 
 import Login from "../index";
+import messages from "../../../../messages";
+import { login } from "../../../../network/api/userApi";
+
+const { labels, placeholders } = messages;
+
+const API = {
+  login() {
+    return login("test222@gmail.com", "Test@123");
+  },
+};
 
 describe("Login page", () => {
-  it("render heading", () => {
+  it("render heading and labels", () => {
     render(<Login isModal={false} />);
     let heading = screen.getByRole("heading", { name: /Login to Canonizer/i });
     expect(heading).toBeInTheDocument();
+    expect(screen.getByText(labels.emailPhone)).toBeInTheDocument();
+    expect(screen.getByText(labels.password)).toBeInTheDocument();
+    expect(screen.getByText("Forgot password").closest("a")).toHaveAttribute(
+      "href",
+      "/"
+    );
+    expect(screen.getByText("Don't have an account?")).toBeVisible();
+    expect(screen.getByText("Register Now")).toBeVisible();
+    expect(screen.getByText("Remember me")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "Enter to the best leaderless consensus building and tracking system in the world."
+      )
+    ).toBeInTheDocument();
   });
 
-  it("render username input", () => {
+  it("render username input field", () => {
     render(<Login isModal={false} />);
-    const inputEl = screen.getByPlaceholderText("Enter Email / Phone Number");
+    const inputEl = screen.getByLabelText(labels.emailPhone);
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute("type", "text");
+    expect(inputEl).toHaveAttribute("placeholder", placeholders.emailPhone);
   });
 
-  it("render password input", () => {
+  it("render password input field", () => {
     render(<Login isModal={false} />);
-    const inputEl = screen.getByPlaceholderText("Enter Password");
+    const inputEl = screen.getByLabelText(labels.password);
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute("type", "password");
+    expect(inputEl).toHaveAttribute("placeholder", placeholders.password);
   });
 
   it("pass valid email to test email input field", async () => {
     render(<Login isModal={false} />);
-    const inputEl = screen.getByPlaceholderText("Enter Email / Phone Number");
-    userEvent.type(inputEl, "test@mail.com");
+    const inputEl = screen.getByLabelText(labels.emailPhone);
+    userEvent.type(inputEl, "rahul.singh@iffort.com");
     await waitFor(() => {
-      expect(inputEl).toHaveValue("test@mail.com");
+      expect(inputEl).toHaveValue("rahul.singh@iffort.com");
       expect(screen.queryByRole("alert")).not.toBeInTheDocument();
     });
   });
 
-  it("pass invalid email to test email", async () => {
+  it("should show error when invalid email enter in field", async () => {
     render(<Login isModal={false} />);
-    const inputEl = screen.getByPlaceholderText("Enter Email / Phone Number");
-    userEvent.type(inputEl, "abcmail.com");
+    const inputEl = screen.getByLabelText(labels.emailPhone);
+    userEvent.type(inputEl, "rahul.singhiffort.com");
     await waitFor(() => {
       expect(screen.getByText("Input is not valid!")).toBeInTheDocument();
     });
   });
 
-  it("pass invalid email to test email input field", async () => {
+  it("blank form should not be submit", async () => {
     render(<Login isModal={false} />);
-    const inputEl = screen.getByPlaceholderText("Enter Email / Phone Number");
-    userEvent.type(inputEl, "test@mail");
-    expect(inputEl).toHaveValue("test@mail");
+    const btnEl = screen.getByTestId("submitButton");
+
+    userEvent.click(btnEl);
+
     await waitFor(() => {
-      expect(screen.queryByRole("alert")).toBeInTheDocument();
-      expect(screen.queryByRole("alert").textContent).toEqual(
-        "Input is not valid!"
-      );
+      expect(
+        screen.queryByText("Please input your Email / Phone Number!")
+      ).toBeVisible();
+      expect(screen.queryByText("Please input your Password!")).toBeVisible();
     });
   });
 
-  it("pass empty form is not submitted", async () => {
-    render(<Login isModal={false} />);
-    const submitBtn = screen.getByTestId("submitButton");
-    userEvent.click(submitBtn);
-    await waitFor(() => {
-      expect(screen.queryByRole("alert")).toBeInTheDocument();
-      expect(screen.queryByRole("alert").textContent).toEqual(
-        "Please input your Email / Phone Number!"
-      );
-      //   expect((screen.queryAllByRole("alert")[0]).textContent).toEqual(
-      //     "Please input your Password!"
-      //   );
-    });
-  });
+  // it("should not login with unregistered email", async () => {
+  //   const { getByTestId } = render(<Login isModal={false} />);
+  //   const resD = { data: { auth: {}, user: {} } };
+
+  //   const res = jest.spyOn(API, "login").mockImplementationOnce(() => {
+  //     return Promise.resolve(resD);
+  //   });
+
+  //   userEvent.click(getByTestId("submitButton"));
+
+  //   await waitFor(() => {
+  //     expect(screen.queryByText("Something went wrong")).toBeInTheDocument();
+  //   });
+
+  //   // jest.mock("../../../../network/api/userApi", () => {
+  //   //   return {
+  //   //     getLoginRes: jest.fn(() => Promise.resolve(resD)),
+  //   //   };
+  //   // });
+
+  //   // // const res = await login("test@gmail.com", "Test@123");
+  //   // await waitFor(() => {
+  //   //   expect(res.status_code).not.toBe(200);
+  //   // });
+  // });
+
+  // it("should not login with valid email and invalid password", async () => {
+  //   const res = await login("rahul.singh@iffort.com", "Test@1234");
+  //   await waitFor(() => {
+  //     expect(res.status_code).not.toBe(200);
+  //   });
+  // });
+
+  // it("should not login with invalid email and valid password", async () => {
+  //   const res = await login("rahul123@test.com", "Test@123");
+  //   await waitFor(() => {
+  //     expect(res.status_code).not.toBe(200);
+  //   });
+  // });
+
+  // it("should get access token from api with valid credential", async () => {
+  //   const res = await login("rahul.singh@iffort.com", "Test@123");
+  //   await waitFor(() => {
+  //     expect(res.status_code).toBe(200);
+  //   });
+  // });
 });
---
+++ src/components/ComponentPages/Registration/__test__/registration.test.tsx
@@ -0,0 +1,186 @@
+import { render, screen, waitFor } from "../../../../utils/testUtils";
+import userEvent from "@testing-library/user-event";
+
+import Registration from "../index";
+import messages from "../../../../messages";
+
+const { labels, placeholders, validations } = messages;
+
+describe("Registration page", () => {
+  it("render heading and labels", () => {
+    render(<Registration isModal={false} />);
+    let heading = screen.getByRole("heading", {
+      name: /Register Now on Canonizer/i,
+    });
+    expect(heading).toBeInTheDocument();
+    expect(screen.getByText("Already have an account?")).toBeVisible();
+    expect(screen.getByText("Login Here")).toBeVisible();
+    expect(screen.getByText(labels.firstName)).toBeInTheDocument();
+    expect(screen.getByText(labels.middleName)).toBeInTheDocument();
+    expect(screen.getByText(labels.lastName)).toBeInTheDocument();
+    expect(screen.getByText(labels.email)).toBeInTheDocument();
+    expect(screen.getByText(labels.phone)).toBeInTheDocument();
+    expect(screen.getByText(labels.registrationPassword)).toBeInTheDocument();
+    expect(screen.getByText(labels.confirmPassword)).toBeInTheDocument();
+    expect(screen.getByText(labels.captcha)).toBeInTheDocument();
+  });
+
+  it("render inputs field and submit button", () => {
+    render(<Registration isModal={false} />);
+    const firstName = screen.getByLabelText(labels.firstName);
+    const middleName = screen.getByLabelText(labels.middleName);
+    const lastName = screen.getByLabelText(labels.lastName);
+    const email = screen.getByLabelText(labels.email);
+    const phone = screen.getByLabelText(labels.phone);
+    const registrationPassword = screen.getByLabelText(
+      labels.registrationPassword
+    );
+    const confirmPassword = screen.getByLabelText(labels.confirmPassword);
+
+    expect(firstName).toBeInTheDocument();
+    expect(firstName).toHaveAttribute("type", "text");
+    expect(firstName).toHaveAttribute("placeholder", placeholders.firstName);
+
+    expect(middleName).toBeInTheDocument();
+    expect(middleName).toHaveAttribute("type", "text");
+    expect(middleName).toHaveAttribute("placeholder", placeholders.middleName);
+
+    expect(lastName).toBeInTheDocument();
+    expect(lastName).toHaveAttribute("type", "text");
+    expect(lastName).toHaveAttribute("placeholder", placeholders.lastName);
+
+    expect(email).toBeInTheDocument();
+    expect(email).toHaveAttribute("type", "text");
+    expect(email).toHaveAttribute("placeholder", placeholders.email);
+
+    expect(phone).toBeInTheDocument();
+    expect(phone).toHaveAttribute("type", "text");
+    expect(phone).toHaveAttribute("placeholder", placeholders.phone);
+
+    expect(registrationPassword).toBeInTheDocument();
+    expect(registrationPassword).toHaveAttribute("type", "password");
+    expect(registrationPassword).toHaveAttribute(
+      "placeholder",
+      placeholders.registrationPassword
+    );
+
+    expect(confirmPassword).toBeInTheDocument();
+    expect(confirmPassword).toHaveAttribute("type", "password");
+    expect(confirmPassword).toHaveAttribute(
+      "placeholder",
+      placeholders.confirmPassword
+    );
+  });
+
+  it("pass valid email to test email input field", async () => {
+    render(<Registration isModal={false} />);
+    const inputEl = screen.getByLabelText(labels.email);
+    userEvent.type(inputEl, "rahul.singh@iffort.com");
+    await waitFor(() => {
+      expect(inputEl).toHaveValue("rahul.singh@iffort.com");
+      expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+    });
+  });
+
+  it("should show error when invalid email enter in field", async () => {
+    render(<Registration isModal={false} />);
+    const inputEl = screen.getByLabelText(labels.email);
+    userEvent.type(inputEl, "rahul.singhiffort.com");
+    await waitFor(() => {
+      expect(
+        screen.queryByText("The input is not valid E-mail!")
+      ).toBeVisible();
+    });
+  });
+
+  it("check phone number length is less than 9 chars", async () => {
+    render(<Registration isModal={false} />);
+    const inputEl = screen.getByLabelText(labels.phone);
+    userEvent.type(inputEl, "12345678");
+    await waitFor(() => {
+      expect(inputEl).toHaveValue("12345678");
+      expect(screen.queryByRole("alert")).toBeInTheDocument();
+      expect(screen.queryByText(validations.phoneLength)).toBeVisible();
+    });
+  });
+
+  it("check phone number length should be min of 9 chars", async () => {
+    render(<Registration isModal={false} />);
+    const inputEl = screen.getByLabelText(labels.phone);
+    userEvent.type(inputEl, "123456789");
+    await waitFor(() => {
+      expect(inputEl).toHaveValue("123456789");
+      expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+    });
+  });
+
+  it("check password minimum length > 8", async () => {
+    render(<Registration isModal={false} />);
+    const inputEl = screen.getByLabelText(labels.password);
+    userEvent.type(inputEl, "1234567");
+    await waitFor(() => {
+      expect(inputEl).toHaveValue("1234567");
+      expect(screen.queryByRole("alert")).toBeInTheDocument();
+      expect(
+        screen.queryByText("Password Should be like Abc@1234.")
+      ).toBeVisible();
+    });
+  });
+
+  it("pass valid password", async () => {
+    render(<Registration isModal={false} />);
+    const inputEl = screen.getByLabelText(labels.password);
+    userEvent.type(inputEl, "Abc@1234");
+    await waitFor(() => {
+      expect(inputEl).toHaveValue("Abc@1234");
+      expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+    });
+  });
+
+  it("pass invalid confirm password", async () => {
+    render(<Registration isModal={false} />);
+    const inputEl = screen.getByLabelText(labels.password);
+    const inputEl2 = screen.getByLabelText(labels.confirmPassword);
+    userEvent.type(inputEl, "Abc@1234");
+    userEvent.type(inputEl2, "Abc@12344");
+    await waitFor(() => {
+      expect(inputEl).toHaveValue("Abc@1234");
+      expect(inputEl2).toHaveValue("Abc@12344");
+      expect(screen.queryByRole("alert")).toBeInTheDocument();
+      expect(
+        screen.queryByText("The two passwords that you entered do not match!")
+      ).toBeVisible();
+    });
+  });
+
+  it("pass valid confirm password", async () => {
+    render(<Registration isModal={false} />);
+    const inputEl = screen.getByLabelText(labels.password);
+    const inputEl2 = screen.getByLabelText(labels.confirmPassword);
+    userEvent.type(inputEl, "Abc@1234");
+    userEvent.type(inputEl2, "Abc@1234");
+    await waitFor(() => {
+      expect(inputEl).toHaveValue("Abc@1234");
+      expect(inputEl2).toHaveValue("Abc@1234");
+      expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+    });
+  });
+
+  it("blank form should not be submit", async () => {
+    render(<Registration isModal={false} />);
+    const btnEl = screen.getByTestId("submitButton");
+
+    userEvent.click(btnEl);
+
+    await waitFor(() => {
+      expect(screen.queryByText("Please input your first name!")).toBeVisible();
+      expect(screen.queryByText("Please input your last name!")).toBeVisible();
+      expect(screen.queryByText("Please input your E-mail!")).toBeVisible();
+      expect(screen.queryByText("Please input your password!")).toBeVisible();
+      expect(screen.queryByText("Please confirm your password!")).toBeVisible();
+      expect(
+        screen.queryByText("Please input the captcha you got!")
+      ).toBeVisible();
+    });
+  });
+});
