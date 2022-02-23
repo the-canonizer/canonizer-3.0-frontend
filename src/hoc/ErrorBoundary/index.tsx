07c8694b7044a323968912f1bfb2db6fca653da1
shahab-ramzan <shahab.ramzan@codedistrict.com>
2022-02-23 11:38:42 +0500

    minor update in errorBoundary

--- src/hoc/ErrorBoundary/index.tsx
+++ src/hoc/ErrorBoundary/index.tsx
@@ -7,13 +7,13 @@ import Image from "next/image";
 import styles from "./errorPage.module.scss";
 
 export default class ErrorBoundary extends Component {
-  state = { hasError: true, redirect: false };
+  state = { hasError: false, redirect: false };
 
   static getDerivedStateFromError(error) {
     if (error.name === "ChunkLoadError") {
       return window.location.reload();
     }
-    return { hasError: false, redirect: false };
+    return { hasError: true, redirect: false };
   }
 
   componentDidCatch(error, info) {
