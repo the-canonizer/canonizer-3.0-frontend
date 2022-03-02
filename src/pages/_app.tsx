d97e5ec0e52fd733933ec0111e5f82704b187db6
shahab-ramzan <shahab.ramzan@codedistrict.com>
2022-03-02 17:10:19 +0500

    accessed store on server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,40 +5,28 @@ import "../assets/fonticons/style.css";
 import "../assets/scss/global.scss";
 import { store } from "../store";
 import { Provider } from "react-redux";
-import type { AppProps } from "next/app";
-import { createWrapper } from "next-redux-wrapper";
 import HeadContentAndPermissionComponent from "../components/common/headContentAndPermisisonCheck";
 import ErrorBoundary from "../hoc/ErrorBoundary";
-
 import GoogleAnalyticScripts from "../firebaseConfig/scripts";
+import React from "react";
+import App, { AppInitialProps, AppContext } from "next/app";
+import { wrapper } from "../store";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GoogleAnalyticScripts />
-      <Provider store={store}>
-        <ErrorBoundary>
-          <HeadContentAndPermissionComponent componentName={Component.name} />
-          <Component {...pageProps} />
-        </ErrorBoundary>
-      </Provider>
-    </>
-  );
+class WrappedApp extends App<AppInitialProps> {
+  public render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <GoogleAnalyticScripts />
+        <Provider store={store}>
+          <ErrorBoundary>
+            <HeadContentAndPermissionComponent componentName={Component.name} />
+            <Component {...pageProps} />
+          </ErrorBoundary>
+        </Provider>
+      </>
+    );
+  }
 }
 
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext: AppContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-
-//   return { ...appProps }
-// }
-
-const makeStore = () => store;
-const wrapper = createWrapper(makeStore);
-
-export default wrapper.withRedux(MyApp);
+export default wrapper.withRedux(WrappedApp);
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,13 @@
 import homePageSlice from "./slices/homePageSlice";
+
+// // reducers
+import Auth from "./slices/authSlice";
+import Tree from "./slices/treeSlice";
+import UiReducer from "./slices/uiSlice";
+
+import { createStore, applyMiddleware } from "redux";
+
+import { MakeStore, createWrapper, Context } from "next-redux-wrapper";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import {
   FLUSH,
@@ -12,9 +21,6 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 // reducers
-import Auth from "./slices/authSlice";
-import Tree from "./slices/treeSlice";
-import UiReducer from "./slices/uiSlice";
 
 let reducers = combineReducers({
   auth: Auth,
@@ -43,6 +49,13 @@ const store = configureStore({
 });
 export type RootState = ReturnType<typeof reducers>;
 export type AppDispatch = typeof store.dispatch;
+export interface State {
+  tree: string;
+}
+
+export const makeStore: MakeStore<any> = (context: Context) =>
+  createStore(persistedReducer);
 
 const persistor = persistStore(store);
 export { persistor, store };
+export const wrapper = createWrapper<any>(makeStore, { debug: true });
