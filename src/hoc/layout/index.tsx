4cd7ac346c023925b6a4891833adb39e74c6efb0
mudassar-cd <mudassar.ali@codedistrict.com>
2022-03-02 18:16:24 +0500

    working on topic details breadcrumb

--- src/hoc/layout/index.tsx
+++ src/hoc/layout/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
+import {Breadcrumb, Typography } from 'antd';
 import useAuthentication from "../../hooks/isUserAuthenticated";
 import Image from "next/image";
-
 import LoggedInHeader from "../../components/common/headers/loggedInHeader";
 import LoggedOutHeader from "../../components/common/headers/loggedOutHeader";
 import Spinner from "../../components/common/spinner/spinner";
@@ -16,6 +16,8 @@ function Layout(props) {
   }));
   const { isUserAuthenticated } = useAuthentication();
 
+ 
+
   useEffect(() => {
     if (isUserAuthenticated) {
       setIsLogin(true);
@@ -23,13 +25,33 @@ function Layout(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [authenticated]);
 
+
   return (
     <>
       <div className={styles.pageWrap}>
         {isLogin ? <LoggedInHeader /> : <LoggedOutHeader />}
         <Spinner>{""}</Spinner>
+        <div className={styles.breadcrumbWrapper}>
+            <Typography.Paragraph className={"mb-0 " + styles.topicTitleStyle}> <span className="bold"> Topic : </span>  Theories of Consciousness </Typography.Paragraph>
+            <div className={styles.breadcrumbLinks}> <span className="bold mr-1"> Camp :  </span>
+              <Breadcrumb>
+                  <Breadcrumb.Item>
+                    <a href=""> Agreement </a>
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Item>
+                    <a href=""> Approachable Via Science </a>
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Item>
+                    <a href=""> Representational Qualia </a>
+                  </Breadcrumb.Item>
+              </Breadcrumb>
+            </div>
+          
+        </div>
+
+
         <div className={styles.contentWrap}>
-          <div className={styles.contentArea}>{props.children}</div>
+          <div className={styles.contentArea}>{props.children} </div>
           <aside className={styles.rightSidebar}>
             <Image
               src="/images/right-sidebar-adv.png"
