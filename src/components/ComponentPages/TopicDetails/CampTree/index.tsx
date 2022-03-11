d370496b8791e1cb4918a09582c8903975c88452
shahab-ramzan <shahab.ramzan@codedistrict.com>
2022-03-11 15:21:21 +0500

    filter-by-score topic tree

--- src/components/ComponentPages/TopicDetails/CampTree/index.tsx
+++ src/components/ComponentPages/TopicDetails/CampTree/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Tree } from "antd";
 import { useSelector } from "react-redux";
 import { RootState } from "src/store";
@@ -6,10 +6,12 @@ import Styles from "../campTree.module.scss";
 
 const { TreeNode } = Tree;
 const CampTree = ({ scrollToCampStatement }) => {
-  const [selectedNodeID, setSelectedNodeID] = useState(null);
-  const { tree } = useSelector((state: RootState) => ({
+  const { tree, filterByScore } = useSelector((state: RootState) => ({
     tree: state?.topicDetails?.tree,
+    filterByScore: state.homePage?.filterObject?.filterByScore,
   }));
+  const [selectedNodeID, setSelectedNodeID] = useState(null);
+  const [scoreFilter, setScoreFilter] = useState(filterByScore);
 
   const onSelect = (selectedKeys) => {
     console.log("selected", selectedKeys);
@@ -21,52 +23,61 @@ const CampTree = ({ scrollToCampStatement }) => {
     }
   };
 
+  useEffect(() => {
+    setScoreFilter(filterByScore);
+  }, [filterByScore]);
+
   const renderTreeNodes = (data: any) =>
     Object.keys(data).map((item) => {
       if (data[item].children) {
-        return (
-          <>
-            <TreeNode
-              title={
-                <>
-                  <div className={"treeListItem " + Styles.treeListItem}>
-                    <span
-                      className={
-                        "treeListItemTitle " + Styles.treeListItemTitle
-                      }
-                    >
-                      {" "}
-                      {data[item].title}
-                    </span>
-                    <span
-                      className={
-                        "treeListItemNumber " + Styles.treeListItemNumber
-                      }
-                    >
-                      {" "}
-                      {data[item].score}
-                    </span>
-                  </div>
-                </>
-              }
-              key={data[item].camp_id}
-            >
-              {selectedNodeID === data[item].camp_id && (
-                <TreeNode
-                  key={"custom"}
-                  title={
-                    <p
-                      onClick={() => {
-                        "supportCamp";
-                      }}
-                    >{`<Start new supporting camp here>`}</p>
-                  }
-                />
-              )}
-              {renderTreeNodes(data[item].children)}
-            </TreeNode>
-          </>
-        );
+        // debugger;
+        if (data[item].score > scoreFilter) {
+          return (
+            <>
+              <TreeNode
+                title={
+                  <>
+                    <div className={"treeListItem " + Styles.treeListItem}>
+                      <span
+                        className={
+                          "treeListItemTitle " + Styles.treeListItemTitle
+                        }
+                      >
+                        {" "}
+                        {data[item].title}
+                      </span>
+                      <span
+                        className={
+                          "treeListItemNumber " + Styles.treeListItemNumber
+                        }
+                      >
+                        {" "}
+                        {data[item].score}
+                      </span>
+                    </div>
+                  </>
+                }
+                key={data[item].camp_id}
+              >
+                {selectedNodeID === data[item].camp_id && (
+                  <TreeNode
+                    key={"custom"}
+                    title={
+                      <p
+                        onClick={() => {
+                          "supportCamp";
+                        }}
+                      >{`<Start new supporting camp here>`}</p>
+                    }
+                  />
+                )}
+                {renderTreeNodes(data[item].children)}
+              </TreeNode>
+            </>
+          );
+        } else {
+          return null;
+        }
       }
       return <TreeNode key={data[item].key} {...data[item]} />;
     });
@@ -77,6 +88,7 @@ const CampTree = ({ scrollToCampStatement }) => {
       defaultExpandedKeys={["1"]}
       onSelect={onSelect}
       autoExpandParent={true}
+      // filterTreeNode={filterTreeNode}
     >
       {tree && renderTreeNodes(tree)}
     </Tree>
