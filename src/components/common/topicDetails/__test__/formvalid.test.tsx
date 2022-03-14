a84ec50de46048e01c4136181b9d41243192674a
wahaj <wahaj.hafeez@codedistrict.com>
2022-03-14 12:34:40 +0500

    test cases of addnews

--- src/components/common/topicDetails/__test__/formvalid.test.tsx
+++ src/components/common/topicDetails/__test__/formvalid.test.tsx
@@ -1,7 +1,18 @@
-import FormData from "../../../testtrees/form";
+import FormData from "../../../addnewsformdata/form";
 import { cleanup, render, screen } from "@testing-library/react";
 
-describe("Should render the app without crashing list", () => {
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+
+afterEach(cleanup);
+describe("Should render Addnews", () => {
   // it("Renders the home page2", () => {
   //   render(<Showtreedetail result={fakedata} />);
 
@@ -11,10 +22,25 @@ describe("Should render the app without crashing list", () => {
   //   ).toBeInTheDocument();
   // });
 
-  it("Renders the home page2", async () => {
-    render(<FormData />);
+  it("Render without crash", async () => {
+    const { container } = render(<FormData />);
+    const formHeading = screen.getByRole("heading", {
+      name: /form/i,
+    });
 
-    const input = screen.getByText("form");
-    expect(input).toBeInTheDocument();
+    const submitButton = screen.getByRole("button", {
+      name: /submit/i,
+    });
+    const cancelButton = screen.getByRole("button", {
+      name: /Cancel/i,
+    });
+
+    expect(container.getElementsByTagName("button")).toHaveLength(2);
+    expect(container.getElementsByTagName("textarea")).toHaveLength(1);
+    expect(container.getElementsByTagName("input")).toHaveLength(2);
+    expect(screen.getByText(/display text/i));
+    expect(screen.getByText(/link/i));
+    expect(submitButton.textContent).toBe("Submit");
+    expect(cancelButton.textContent).toBe("Cancel");
   });
 });
