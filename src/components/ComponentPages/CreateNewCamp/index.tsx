da877ca48abff240f7d17c9e06b5e1a61ed9f9d6
Rahul Kumar Singh <rahul.singh@iffort.com>
2022-03-28 11:30:00 +0530

    camp create changes

--- src/components/ComponentPages/CreateNewCamp/index.tsx
+++ src/components/ComponentPages/CreateNewCamp/index.tsx
@@ -27,6 +27,7 @@ const CreateNewCamp = ({
   const [initialValue, setInitialValues] = useState(initialValues);
   const [parentCamp, setParentCamps] = useState(parentCamps);
   const [campNickName, setCampNickName] = useState(campNickNames);
+  const [crCamp, setCrCamp] = useState({});
 
   const router = useRouter();
   const [form] = Form.useForm();
@@ -92,6 +93,16 @@ const CreateNewCamp = ({
     router.push("/");
   };
 
+  const onValuesChange = (changed, all) => {
+    if (changed.parent_camp_num) {
+      const currentCamp = parentCamp.filter(
+        (c) => c.camp_num === changed.parent_camp_num
+      );
+      setCrCamp(currentCamp[0]);
+      console.log(`crCamp`, currentCamp, crCamp, changed, parentCamp);
+    }
+  };
+
   return (
     <Fragment>
       <CreateNewCampUI
@@ -103,6 +114,8 @@ const CreateNewCamp = ({
         nickNameList={nickNameList}
         parentCamp={parentCamp}
         campNickName={campNickName}
+        onValuesChange={onValuesChange}
+        crCamp={crCamp}
       />
     </Fragment>
   );
