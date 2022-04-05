f7ee4ada39fe29ced245d95e6ce9ba844cfc68f0
adeelahmed-codedistrict <adeel.ahmed@codedistrict.com>
2022-04-05 17:06:31 +0500

    -- add edit news screens and sidebar modifications --

--- src/pages/news/add-news.tsx
+++ src/pages/news/add-news.tsx
@@ -2,7 +2,7 @@ import { Row, Col, Card, Checkbox, Form, Input, Badge, Button } from "antd";
 
 import Layout from "../../hoc/layout";
 import SideBar from "../../components/ComponentPages/Home/SideBar";
-import styles from "./editNews.module.scss";
+import styles from "./addEditNews.module.scss";
 
 const { TextArea } = Input;
 
@@ -14,95 +14,27 @@ function AddNews() {
           <SideBar />
         </aside>
         <div className="pageContentWrap">
-          <Card title="Edit News" className={styles.newsCard}>
+          <Card title="Add News" className={styles.card}>
             <Form layout="vertical">
-              <Card className={styles.innerForm} bordered={false}>
-                <div className={styles.countBadge}>
-                  <Badge className={styles.countRowEdit}>1</Badge>
-                </div>
-                <Row gutter={28}>
-                  <Col xl={14} md={24} xs={24}>
-                    <div className={styles.textAreaForm}>
-                      <Form.Item label="Display Text ( Limit 256 chars )">
-                        <TextArea
-                          placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"'
-                          autoSize={{ minRows: 6, maxRows: 5 }}
-                        />
-                      </Form.Item>
-                    </div>
-                  </Col>
+              <Row gutter={28}>
+                <Col xl={14} md={24} xs={24}>
+                  <Form.Item label={<>Display Text <small>(Limit 256 chars)</small></>} className={styles.formItem}>
+                    <TextArea size="large" placeholder='New Video: "Consciousness:Not a Hard Problem Just a Color Problem"' rows={6} />
+                  </Form.Item>
+                </Col>
 
-                  <Col xl={10} md={24} xs={24}>
-                    <div className={styles.formLink}>
-                      <Form.Item label="Link ( Limit 2000 chars )">
-                        <Input placeholder="http:canonizer.com/videos/conciousness/" />
-                      </Form.Item>
-                      <Form.Item>
-                        <Checkbox>Available For Child camps</Checkbox>
-                      </Form.Item>
-                    </div>
-                  </Col>
-                </Row>
-              </Card>
-              {/* <Card className="inner-form">
-                <div className="count-badge">
-                  <Badge className="count-row-edit ">2</Badge>
-                </div>
-                <Row gutter={28}>
-                  <Col xl={14} md={24} xs={24}>
-                    <div className="textarea-form">
-                      <Form.Item label="Display Text ( Limit 256 chars )">
-                        <TextArea
-                          placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"'
-                          autoSize={{ minRows: 6, maxRows: 5 }}
-                        />
-                      </Form.Item>
-                    </div>
-                  </Col>
-
-                  <Col xl={10} md={24} xs={24}>
-                    <div className="form-link">
-                      <Form.Item label="Link ( Limit 2000 chars )">
-                        <Input placeholder="http:canonizer.com/videos/conciousness/" />
-                      </Form.Item>
-                      <Form.Item>
-                        <Checkbox>Available For Child camps</Checkbox>
-                      </Form.Item>
-                    </div>
-                  </Col>
-                </Row>
-              </Card>
-              <Card className="inner-form">
-                <div className="count-badge">
-                  <Badge className="count-row-edit ">3</Badge>
-                </div>
-                <Row gutter={28}>
-                  <Col xl={14} md={24} xs={24}>
-                    <div className="textarea-form">
-                      <Form.Item label="Display Text ( Limit 256 chars )">
-                        <TextArea
-                          placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"'
-                          autoSize={{ minRows: 6, maxRows: 5 }}
-                        />
-                      </Form.Item>
-                    </div>
-                  </Col>
-
-                  <Col xl={10} md={24} xs={24}>
-                    <div className="form-link">
-                      <Form.Item label="Link ( Limit 2000 chars )">
-                        <Input placeholder="http:canonizer.com/videos/conciousness/" />
-                      </Form.Item>
-                      <Form.Item>
-                        <Checkbox>Available For Child camps</Checkbox>
-                      </Form.Item>
-                    </div>
-                  </Col>
-                </Row>
-              </Card> */}
-              <div className={styles.editNewsButtons}>
-                <Button className={styles.submitBtn}>Submit</Button>
-                <Button className={styles.cancelBtn}>Cancel</Button>
+                <Col xl={10} md={24} xs={24}>
+                  <Form.Item label={<>Link <small>(Limit 2000 chars)</small></>} className={`${styles.formItem} mb-3`}>
+                    <Input size="large" placeholder="http:canonizer.com/videos/conciousness/" />
+                  </Form.Item>
+                  <Form.Item className={styles.formItemCheckbox}>
+                    <Checkbox>Available for child camps</Checkbox>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <div>
+                <Button size="large" className="btn-orange mr-3">Create News</Button>
+                <Button type="ghost" size="large" >Cancel</Button>
               </div>
             </Form>
           </Card>
--- src/pages/news/edit-news.tsx
+++ src/pages/news/edit-news.tsx
@@ -2,7 +2,7 @@ import { Row, Col, Card, Checkbox, Form, Input, Badge, Button } from "antd";
 
 import Layout from "../../hoc/layout";
 import SideBar from "../../components/ComponentPages/Home/SideBar";
-import styles from "./editNews.module.scss";
+import styles from "./addEditNews.module.scss";
 
 const { TextArea } = Input;
 
@@ -14,89 +14,68 @@ function EditNews() {
           <SideBar />
         </aside>
         <div className="pageContentWrap">
-          <Card title="Edit News" className={styles.newsCard}>
+          <Card title="Edit News" className={styles.card}>
             <Form layout="vertical">
               <Card className={styles.newsFormFieldsCard} bordered={false}>
                 <Badge className={styles.newsFormFieldsCardCounter}>1</Badge>
                 <Row gutter={28}>
                   <Col xl={14} md={24} xs={24}>
-                      <Form.Item label="Display Text ( Limit 256 chars )" className={styles.textArea}>
-                        <TextArea
-                          placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"'
-                          autoSize={{ minRows: 6, maxRows: 5 }}
-                        />
+                      <Form.Item label={<>Display Text <small>(Limit 256 chars)</small></>} className={styles.formItem}>
+                        <TextArea placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"' rows={6} />
                       </Form.Item>
                   </Col>
 
                   <Col xl={10} md={24} xs={24}>
-                      <Form.Item label="Link ( Limit 2000 chars )" className={styles.formLink}>
-                        <Input placeholder="http:canonizer.com/videos/conciousness/" />
-                      </Form.Item>
-                      <Form.Item>
-                        <Checkbox>Available For Child camps</Checkbox>
-                      </Form.Item>
+                    <Form.Item label={<>Link <small>(Limit 2000 chars)</small></>} className={`${styles.formItem} mb-3`}>
+                      <Input size="large" placeholder="http:canonizer.com/videos/conciousness/" />
+                    </Form.Item>
+                    <Form.Item className={styles.formItemCheckbox}>
+                      <Checkbox>Available For Child camps</Checkbox>
+                    </Form.Item>
                   </Col>
                 </Row>
               </Card>
-              {/* <Card className="inner-form">
-                <div className="count-badge">
-                  <Badge className="count-row-edit ">2</Badge>
-                </div>
+              <Card className={styles.newsFormFieldsCard} bordered={false}>
+                <Badge className={styles.newsFormFieldsCardCounter}>2</Badge>
                 <Row gutter={28}>
                   <Col xl={14} md={24} xs={24}>
-                    <div className="textarea-form">
-                      <Form.Item label="Display Text ( Limit 256 chars )">
-                        <TextArea
-                          placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"'
-                          autoSize={{ minRows: 6, maxRows: 5 }}
-                        />
+                      <Form.Item label={<>Display Text <small>(Limit 256 chars)</small></>} className={styles.formItem}>
+                        <TextArea placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"' rows={6} />
                       </Form.Item>
-                    </div>
                   </Col>
 
                   <Col xl={10} md={24} xs={24}>
-                    <div className="form-link">
-                      <Form.Item label="Link ( Limit 2000 chars )">
-                        <Input placeholder="http:canonizer.com/videos/conciousness/" />
-                      </Form.Item>
-                      <Form.Item>
-                        <Checkbox>Available For Child camps</Checkbox>
-                      </Form.Item>
-                    </div>
+                    <Form.Item label={<>Link <small>(Limit 2000 chars)</small></>} className={`${styles.formItem} mb-3`}>
+                      <Input size="large" placeholder="http:canonizer.com/videos/conciousness/" />
+                    </Form.Item>
+                    <Form.Item className={styles.formItemCheckbox}>
+                      <Checkbox>Available For Child camps</Checkbox>
+                    </Form.Item>
                   </Col>
                 </Row>
               </Card>
-              <Card className="inner-form">
-                <div className="count-badge">
-                  <Badge className="count-row-edit ">3</Badge>
-                </div>
+              <Card className={styles.newsFormFieldsCard} bordered={false}>
+                <Badge className={styles.newsFormFieldsCardCounter}>3</Badge>
                 <Row gutter={28}>
                   <Col xl={14} md={24} xs={24}>
-                    <div className="textarea-form">
-                      <Form.Item label="Display Text ( Limit 256 chars )">
-                        <TextArea
-                          placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"'
-                          autoSize={{ minRows: 6, maxRows: 5 }}
-                        />
+                      <Form.Item label={<>Display Text <small>(Limit 256 chars)</small></>} className={styles.formItem}>
+                        <TextArea placeholder='New Video:"Consciousness:Not a Hard Problem Just a Color Problem"' rows={6} />
                       </Form.Item>
-                    </div>
                   </Col>
 
                   <Col xl={10} md={24} xs={24}>
-                    <div className="form-link">
-                      <Form.Item label="Link ( Limit 2000 chars )">
-                        <Input placeholder="http:canonizer.com/videos/conciousness/" />
-                      </Form.Item>
-                      <Form.Item>
-                        <Checkbox>Available For Child camps</Checkbox>
-                      </Form.Item>
-                    </div>
+                    <Form.Item label={<>Link <small>(Limit 2000 chars)</small></>} className={`${styles.formItem} mb-3`}>
+                      <Input size="large" placeholder="http:canonizer.com/videos/conciousness/" />
+                    </Form.Item>
+                    <Form.Item className={styles.formItemCheckbox}>
+                      <Checkbox>Available For Child camps</Checkbox>
+                    </Form.Item>
                   </Col>
                 </Row>
-              </Card> */}
-              <div className={styles.editNewsButtons}>
-                <Button className="btn-orange">Submit</Button>
-                <Button >Cancel</Button>
+              </Card>
+              <div>
+                <Button size="large" className="btn-orange mr-3">Submit</Button>
+                <Button type="ghost" size="large" >Cancel</Button>
               </div>
             </Form>
           </Card>
