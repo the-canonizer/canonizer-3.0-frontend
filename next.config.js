08a02ca3d8a46359c915d87915e557607b041feb
shahab-ramzan <shahab.ramzan@codedistrict.com>
2022-04-29 15:50:24 +0500

    config updated

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    images: {
-      domains: ["api3.canonizer.com","api3.canonizer.comfa fa-facebook"],
-    },
-  };
+  images: {
+    domains: ["api3.canonizer.com"],
+  },
+};
