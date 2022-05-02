8513ca21b796e45261f9e50ecbee1dbdb1a8fae7
Rahul Kumar Singh <rahul.singh@iffort.com>
2022-05-02 11:06:25 +0530

    api call token code refactor

--- src/components/ComponentPages/CampForum/Create.tsx
+++ src/components/ComponentPages/CampForum/Create.tsx
@@ -1,6 +1,3 @@
-import { Fragment } from "react";
-import { useRouter } from "next/router";
-
 import CreateThread from "./UI/ThreadFormUI";
 import ThreadSidebar from "./UI/sidebar";
 
@@ -12,25 +9,21 @@ const Threads = ({
   form,
   initialValue,
   paramsList,
-}) => {
-  return (
-    <Fragment>
-      <div className="d-flex">
-        <ThreadSidebar />
-        <div className="pageContentWrap">
-          <CreateThread
-            onFinish={onFinish}
-            onCancel={onCancelCreateThread}
-            form={form}
-            initialValue={initialValue}
-            nickNameList={nickNameList}
-            isThreadUpdate={isThreadUpdate}
-            paramsList={paramsList}
-          />
-        </div>
-      </div>
-    </Fragment>
-  );
-};
+}) => (
+  <div className="d-flex">
+    <ThreadSidebar />
+    <div className="pageContentWrap">
+      <CreateThread
+        onFinish={onFinish}
+        onCancel={onCancelCreateThread}
+        form={form}
+        initialValue={initialValue}
+        nickNameList={nickNameList}
+        isThreadUpdate={isThreadUpdate}
+        paramsList={paramsList}
+      />
+    </div>
+  </div>
+);
 
 export default Threads;
