55a1e489679292381430f398f90602726d75a514
wahaj <wahaj.hafeez@codedistrict.com>
2022-05-06 16:21:42 +0500

    add 2 tooltips

--- src/components/ComponentPages/TopicDetails/CampTreeCard/index.tsx
+++ src/components/ComponentPages/TopicDetails/CampTreeCard/index.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Collapse } from "antd";
+import { Checkbox, Collapse, Popover } from "antd";
 import CampTree from "../CampTree";
 import Link from "next/link";
 
@@ -9,6 +9,25 @@ import { useRouter } from "next/router";
 import { subscribeToCampApi } from "../../../../network/api/campDetailApi";
 import { useSelector } from "react-redux";
 import { RootState } from "src/store";
+
+const addContent = (
+  <>
+    <div className={styles.addSupportText}>
+      {/* <Title level={5}>Score Value </Title> */}
+      <p>
+        This section is a table of contents for this topic. It is in outline or
+        tree form, with supporting sub camps indented from the parent camp. If
+        you are in a sub camp, you are also counted in all parent camps
+        including the agreement camp at the top. The numbers are canonized
+        scores derived from the people in the camps based on your currently
+        selected canonizer on the side bar. The camps are sorted according to
+        these canonized scores. Each entry is a link to the camp page which can
+        contain a statement of belief. The green line indicates the camp page
+        you are currently on and the statement below is for that camp.
+      </p>
+    </div>
+  </>
+);
 
 const { Panel } = Collapse;
 
@@ -71,6 +90,10 @@ const CampTreeCard = ({ scrollToCampStatement, getSelectedNode }) => {
                 <i className={"icon-fi-document " + styles.iconMr} /> Add News
               </a>
             </Link>
+
+            <Popover content={addContent} placement="left">
+              <i className="icon-info info-tooltip"></i>
+            </Popover>
           </>
         }
       >
