84787d4baae66eefe3494ff8b523769aa385c146
shahab-ramzan <shahab.ramzan@codedistrict.com>
2022-05-06 16:04:40 +0500

    subscribe to a camp and topic

--- src/components/ComponentPages/TopicDetails/index.tsx
+++ src/components/ComponentPages/TopicDetails/index.tsx
@@ -40,6 +40,7 @@ const TopicDetails = () => {
 
   const [loadingIndicator, setLoadingIndicator] = useState(false);
   const [getTreeLoadingIndicator, setGetTreeLoadingIndicator] = useState(false);
+
   const router = useRouter();
   const dispatch = useDispatch();
   const { asof, asofdate, algorithm, newsFeed, topicRecord, campRecord } =
@@ -51,7 +52,10 @@ const TopicDetails = () => {
       topicRecord: state?.topicDetails?.currentTopicRecord,
       campRecord: state?.topicDetails?.currentCampRecord,
     }));
-
+  const [campSubscriptionStatus, setCampSubscriptionStatus] = useState(
+    !!campRecord?.campSubscriptionId
+  );
+  const [topicSubscriptionStatus, setTopicSubscriptionStatus] = useState(true);
   useEffect(() => {
     async function getTreeApiCall() {
       setGetTreeLoadingIndicator(true);
@@ -81,6 +85,11 @@ const TopicDetails = () => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [asofdate, algorithm, +router?.query?.camp[1]?.split("-")[0]]);
+
+  useEffect(() => {
+    setCampSubscriptionStatus(!!campRecord?.campSubscriptionId);
+    setTopicSubscriptionStatus(!!topicRecord?.topicSubscriptionId);
+  }, [campRecord?.campSubscriptionId, topicRecord?.topicSubscriptionId]);
 
   const scrollToCampStatement = () => {
     myRefToCampStatement.current?.scrollIntoView({ behavior: "smooth" });
@@ -117,10 +126,10 @@ const TopicDetails = () => {
 
     const data = {
       message: null,
-      topic_num: topicRecord[0]?.topic_num,
-      topic_name: topicRecord[0]?.topic_name,
-      camp_name: topicRecord[0]?.camp_name,
-      parent_camp_num: topicRecord[0]?.camp_num,
+      topic_num: topicRecord?.topic_num,
+      topic_name: topicRecord?.topic_name,
+      camp_name: topicRecord?.camp_name,
+      parent_camp_num: topicRecord?.camp_num,
     };
 
     const topicName = topicRecord?.topic_name.replaceAll(" ", "-");
@@ -139,9 +148,9 @@ const TopicDetails = () => {
     // const data = {
     //   message: null,
     //   topic_num: queryParams.camp[0],
-    //   topic_name: topicRecord[0]?.topic_name,
-    //   camp_name: topicRecord[0]?.camp_name,
-    //   parent_camp_num: topicRecord[0]?.camp_num,
+    //   topic_name: topicRecord?.topic_name,
+    //   camp_name: topicRecord?.camp_name,
+    //   parent_camp_num: topicRecord?.camp_num,
     // };
     // setCurrentTopics(data);
     const topicName = topicRecord?.topic_name.replaceAll(" ", "-");
@@ -152,15 +161,17 @@ const TopicDetails = () => {
     });
   };
 
-  const campScribe = (e) => {
+  const campOrTopicScribe = (isTopic: Boolean) => {
     const reqBody = {
       topic_num: campRecord.topic_num,
-      camp_num: campRecord.camp_num,
-      checked: true,
-      subscription_id: campRecord.campSubscriptionId,
+      camp_num: isTopic ? 0 : campRecord.camp_num,
+      checked: isTopic ? !topicSubscriptionStatus : !campSubscriptionStatus,
+      subscription_id: isTopic
+        ? topicRecord?.topicSubscriptionId
+        : campRecord.campSubscriptionId,
     };
 
-    subscribeToCampApi(reqBody);
+    subscribeToCampApi(reqBody, isTopic);
   };
 
   const campForumDropdownMenu = (
@@ -174,12 +185,17 @@ const TopicDetails = () => {
           </a>
         </Link>
       </Menu.Item>
-      <Menu.Item icon={<i className="icon-subscribe"></i>}>
+      <Menu.Item
+        icon={<i className="icon-subscribe"></i>}
+        onClick={() => campOrTopicScribe(true)}
+        style={topicSubscriptionStatus ? { cursor: "progress" } : null}
+      >
         Subscribe to Entire Topic
       </Menu.Item>
       <Menu.Item
         icon={<i className="icon-subscribe"></i>}
-        onClick={() => campScribe()}
+        onClick={() => campOrTopicScribe(false)}
+        style={campSubscriptionStatus ? { cursor: "progress" } : null}
       >
         Subscribe to the Camp
       </Menu.Item>
