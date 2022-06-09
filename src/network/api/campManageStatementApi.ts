160e17bc541fc3fbfb6d773f5eb228f07b8a8e11
wahaj <wahaj.hafeez@codedistrict.com>
2022-06-09 20:03:50 +0500

    show valid error msgs

--- src/network/api/campManageStatementApi.ts
+++ src/network/api/campManageStatementApi.ts
@@ -33,7 +33,9 @@ export const updateStatementApi = async (body) => {
     );
     return res;
   } catch (error) {
-    message.error(error?.error?.data?.message);
+    Object.entries(error?.error?.data?.error).forEach((msg) => {
+      message?.error(msg[1][0]);
+    });
     return error?.error?.data;
   }
 };
