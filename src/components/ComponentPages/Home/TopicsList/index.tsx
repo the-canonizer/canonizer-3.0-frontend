57c92db466d10968063d5f7248f009e84ec63c69
shahab-ramzan <shahab.ramzan@codedistrict.com>
2022-06-15 19:47:24 +0500

    minor update for include a review filter

--- src/components/ComponentPages/Home/TopicsList/index.tsx
+++ src/components/ComponentPages/Home/TopicsList/index.tsx
@@ -272,7 +272,7 @@ const TopicsList = () => {
                     >
                       <Text className={styles.text}>
                         {isReview
-                          ? item?.tree_structure_1_review_title
+                          ? item?.tree_structure[1].review_title
                           : item?.topic_name}
                       </Text>
                       <Tag className={styles.tag}>
