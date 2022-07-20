a43a056a1be1e013f333d1a1a619f7f8f016a856
wahaj <wahaj.hafeez@codedistrict.com>
2022-07-20 13:35:57 +0500

    in progress

---
+++ src/pages/topic-history/[...camp].tsx
@@ -0,0 +1,16 @@
+import CampHistory from "../../components/ComponentPages/CampHistory";
+
+import Layout from "../../hoc/layout";
+
+const CampHistoryPage = ({}) => {
+  return (
+    <>
+      <Layout>
+        <CampHistory />
+      </Layout>
+    </>
+  );
+};
+
+CampHistoryPage.displayName = "CampHistoryPage";
+export default CampHistoryPage;
