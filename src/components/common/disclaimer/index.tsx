9ed8bb63968a3ec9fa8613b27a0b448c1f78240f
Rahul Kumar Singh <rahul.singh@iffort.com>
2022-08-08 12:10:52 +0530

    disclaimer msg added.

---
+++ src/components/common/disclaimer/index.tsx
@@ -0,0 +1,29 @@
+import { Card } from "antd";
+import Link from "next/link";
+
+import styles from "./disclaimer.module.scss";
+
+import useAuthenticated from "../../../hooks/isUserAuthenticated";
+import { useEffect, useState } from "react";
+
+const DisclaimerMsg = () => {
+  const auth = useAuthenticated();
+
+  const [isLooged, setIsLogged] = useState(auth);
+
+  useEffect(() => setIsLogged(auth), [auth]);
+
+  return isLooged ? (
+    <Card className={styles.card_wrapper}>
+      This is not the live system. Any data submitted here is for testing
+      purposes only and will be lost. Please go to{" "}
+      <a href="https://canonizer.com/" target="_blank" rel="noreferrer">
+        canonizer.com
+      </a>
+    </Card>
+  ) : (
+    ""
+  );
+};
+
+export default DisclaimerMsg;
