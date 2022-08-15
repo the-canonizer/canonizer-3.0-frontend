21bf18bf29e09c7c7b8ed549de03d9b06a68bca9
shahab-ramzan <shahab.ramzan@codedistrict.com>
2022-08-15 16:51:06 +0500

    can-877

--- src/components/ComponentPages/Home/SideBar/index.tsx
+++ src/components/ComponentPages/Home/SideBar/index.tsx
@@ -4,15 +4,45 @@ import { RootState } from "src/store";
 import TopicsFilter from "../../../common/topicsFilter";
 import CampRecentActivities from "../CampRecentActivities";
 import NewsFeedsCard from "../../TopicDetails/NewsFeedsCard";
+import { Button, Drawer } from "antd";
+import { useState } from "react";
+
 export default function HomeSideBar({ onCreateCamp = () => {} }) {
   const router = useRouter();
   const { newsFeed } = useSelector((state: RootState) => ({
     newsFeed: state?.topicDetails?.newsFeed,
   }));
 
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <>
-      <TopicsFilter onCreateCamp={onCreateCamp} />
+      {" "}
+      {typeof window !== "undefined" && window.innerWidth > 767 ? (
+        <TopicsFilter onCreateCamp={onCreateCamp} />
+      ) : (
+        <>
+          <Button type="primary" onClick={showDrawer}>
+            Filters
+          </Button>
+          <Drawer
+            title="Filters"
+            placement="right"
+            onClose={onClose}
+            visible={visible}
+          >
+            <TopicsFilter onCreateCamp={onCreateCamp} />
+          </Drawer>
+        </>
+      )}
       {typeof window !== "undefined" && window.innerWidth > 767 && (
         <>
           {router.asPath.includes("topic") && <CampRecentActivities />}
