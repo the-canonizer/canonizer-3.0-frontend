4e5a6ee73d30ee0dbd5403a16d2c412013022587
Rahul Kumar Singh <rahul.singh@iffort.com>
2022-08-23 13:26:15 +0530

    add state for hide/show create camp btn.

--- src/store/slices/filtersSlice.ts
+++ src/store/slices/filtersSlice.ts
@@ -15,6 +15,7 @@ export const filtersSlice = createSlice({
       search: "",
       includeReview: false,
     },
+    currentCamp: null,
   },
   reducers: {
     setFilterCanonizedTopics: (state, action) => {
@@ -30,13 +31,16 @@ export const filtersSlice = createSlice({
         ...action.payload,
       };
     },
+    setCurrentCamp: (state, action) => {
+      state.currentCamp = action.payload;
+    },
   },
 });
 
 export const {
   setFilterCanonizedTopics,
-
   setIsReviewCanonizedTopics,
+  setCurrentCamp,
 } = filtersSlice.actions;
 
 export default filtersSlice.reducer;
