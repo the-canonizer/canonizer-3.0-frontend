704de92b89e81c5ac32d2f42b6c0ad5954129bd4
shahab Ramzan <96526562+shahab-ramzan@users.noreply.github.com>
2022-10-12 18:01:48 +0500

    build analyzer added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports =withBundleAnalyzer( {
   images: {
     domains: ["api3.canonizer.com", "canonizer-public-file.s3.us-east-2.amazonaws.com"],
   },
-};
+});
