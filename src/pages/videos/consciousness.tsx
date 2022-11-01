acf6ad99860e2455c0ddbaaa5fa083faa6ea031c
shahab Ramzan <96526562+shahab-ramzan@users.noreply.github.com>
2022-11-01 14:02:51 +0500

    videos/consciousness route added

--- src/pages/videos/consciousness.tsx
+++ src/pages/videos/consciousness.tsx
@@ -1,4 +1,4 @@
-import Layout from "../hoc/layout";
+import Layout from "../../hoc/layout";
 import React, { ReactElement } from 'react'
 import CanonVideos from "@/components/ComponentPages/canonVideos";
 
