0ef89abc5c4c01ce05956976166879af71fc2bfb
Rahul Kumar Singh <rahul.singh@iffort.com>
2022-11-18 12:39:19 +0530

    CAN-1537 local file path related changes done.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   images: {
-    domains: ["api3.canonizer.com", "canonizer-public-file.s3.us-east-2.amazonaws.com"],
+    domains: [
+      "api3.canonizer.com",
+      "canonizer-public-file.s3.us-east-2.amazonaws.com",
+      "canonizer.com",
+      "beta.canonizer.com",
+      "canonizer3.canonizer.com",
+    ],
   },
 };
