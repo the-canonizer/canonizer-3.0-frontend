daa5be858f51a79fce4a5ee853a233bcac213490
Rahul Kumar Singh <rahul.singh@iffort.com>
2022-12-07 16:40:33 +0530

    ticket #610 ads code for beta has been added in the both sidebar.

--- src/hoc/layout/index.tsx
+++ src/hoc/layout/index.tsx
@@ -6,6 +6,7 @@ import Spinner from "../../components/common/spinner/spinner";
 import styles from "./layout.module.scss";
 import Footer from "../../components/common/footer";
 import Link from "next/link";
+import Script from "next/script";
 
 function Layout(props) {
   const { isUserAuthenticated } = useAuthentication();
@@ -18,12 +19,14 @@ function Layout(props) {
         <div className={styles.contentWrap}>
           <div className={styles.contentArea}>{props.children} </div>
           <aside className={styles.rightSidebar}>
-            <Image
-              src="/images/right-sidebar-adv.png"
-              width={200}
-              height={635}
-              alt=""
-            />
+            <Script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6646446076038181"
+            ></Script>
+
+            <Script>
+              {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+            </Script>
           </aside>
         </div>
         <Footer />
