99e0c1667b1fe513eb75175e430dc7186518f608
vikasrana <vikas.rana@talentelgia.in>
2022-12-15 16:03:47 +0530

    Tciket #603
    Editor fixes

--- src/components/ComponentPages/Statement/AddOrManage/index.tsx
+++ src/components/ComponentPages/Statement/AddOrManage/index.tsx
@@ -62,12 +62,16 @@ import {
 } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftToHtml from "draftjs-to-html";
-import htmlToDraft from "html-to-draftjs";
+// import htmlToDraft from "html-to-draftjs";
 // import { Editor } from 'react-draft-wysiwyg';
 const Editor = dynamic(
   () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
   { ssr: false }
 );
+let htmlToDraft = null;
+if (typeof window === 'object') {
+    htmlToDraft = require('html-to-draftjs').default;
+}
 const { Text } = Typography;
 
 const { campAboutUrlRule, summaryRule, keywordsRule, patterns, validations } =
