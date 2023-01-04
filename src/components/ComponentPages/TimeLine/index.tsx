f216eab73c6237691937ae4acda33a587ab347e0
shahab Ramzan <96526562+shahab-ramzan@users.noreply.github.com>
2023-01-04 16:39:26 +0500

    d3 and race card added

---
+++ src/components/ComponentPages/TimeLine/index.tsx
@@ -0,0 +1,181 @@
+import React, { useState } from "react";
+import RacingBarChart from "./RacingBarChart.js";
+import useInterval from "./useInterval";
+// import "./App.css";
+
+const getRandomIndex = (array) => {
+  return Math.floor(array.length * Math.random());
+};
+
+function TimeLine() {
+  const [iteration, setIteration] = useState(0);
+  const [start, setStart] = useState(false);
+  const [data, setData] = useState([
+    {
+      name: "alpha",
+      value: 20,
+      color: "#f4efd3",
+    },
+    {
+      name: "beta",
+      value: 15,
+      color: "#cccccc",
+    },
+    {
+      name: "charlie",
+      value: 10,
+      color: "#c2b0c9",
+    },
+    {
+      name: "delta",
+      value: 25,
+      color: "#9656a1",
+    },
+    {
+      name: "echo",
+      value: 30,
+      color: "#fa697c",
+    },
+    {
+      name: "foxtrot",
+      value: 35,
+      color: "#fcc169",
+    },
+  ]);
+
+  const mockData = {
+    1: [
+      {
+        name: "alpha",
+        value: 10,
+        color: "#f4efd3",
+      },
+      {
+        name: "beta",
+        value: 15,
+        color: "#cccccc",
+      },
+      {
+        name: "charlie",
+        value: 20,
+        color: "#c2b0c9",
+      },
+      {
+        name: "delta",
+        value: 25,
+        color: "#9656a1",
+      },
+      {
+        name: "echo",
+        value: 30,
+        color: "#fa697c",
+      },
+      {
+        name: "foxtrot",
+        value: 35,
+        color: "#fcc169",
+      },
+    ],
+    2: [
+      {
+        name: "alpha",
+        value: 35,
+        color: "#f4efd3",
+      },
+      {
+        name: "beta",
+        value: 15,
+        color: "#cccccc",
+      },
+      {
+        name: "charlie",
+        value: 20,
+        color: "#c2b0c9",
+      },
+      {
+        name: "delta",
+        value: 25,
+        color: "#9656a1",
+      },
+      {
+        name: "echo",
+        value: 30,
+        color: "#fa697c",
+      },
+      {
+        name: "foxtrot",
+        value: 10,
+        color: "#fcc169",
+      },
+    ],
+    3: [
+      {
+        name: "alpha",
+        value: 10,
+        color: "#f4efd3",
+      },
+      {
+        name: "beta",
+        value: 15,
+        color: "#cccccc",
+      },
+      {
+        name: "charlie",
+        value: 20,
+        color: "#c2b0c9",
+      },
+      {
+        name: "delta",
+        value: 35,
+        color: "#9656a1",
+      },
+      {
+        name: "echo",
+        value: 30,
+        color: "#fa697c",
+      },
+      {
+        name: "foxtrot",
+        value: 25,
+        color: "#fcc169",
+      },
+    ],
+  };
+
+  const events = Object.keys(mockData);
+
+  let iterationCount = 0;
+
+  useInterval(() => {
+    if (start && events.length > iteration) {
+      // const randomIndex = getRandomIndex(data);
+      // setData(
+      //   data.map((entry, index) =>
+      //     index === randomIndex
+      //       ? {
+      //           ...entry,
+      //           value: entry.value + 10,
+      //         }
+      //       : entry
+      //   )
+      // );
+      setData(mockData[events[iteration]]);
+      console.log(data);
+      setIteration(iteration + 1);
+      iterationCount++;
+    }
+  }, 1000);
+
+  return (
+    <React.Fragment>
+      <h1>Racing Bar Chart</h1>
+      <RacingBarChart data={data} />
+      <button onClick={() => setStart(!start)}>
+        {start ? "Stop the race" : "Start the race!"}
+      </button>
+      <p>Iteration: {iteration}</p>
+    </React.Fragment>
+  );
+}
+
+export default TimeLine;
---
+++ src/pages/timeline.tsx
@@ -0,0 +1,12 @@
+import TimeLine from "@/components/ComponentPages/TimeLine";
+
+const TimeLinePage = () => {
+  return (
+    <div>
+      <h1>Time Line</h1>
+      <TimeLine />
+    </div>
+  );
+};
+
+export default TimeLinePage;
