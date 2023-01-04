c4013298e21cfed5f65eea7c01a0b606e03876d6
Rahul Kumar Singh <rahul.singh@iffort.com>
2023-01-04 17:14:29 +0530

    skeleton implementation.

--- src/components/common/customSkelton/index.tsx
+++ src/components/common/customSkelton/index.tsx
@@ -1,29 +1,43 @@
-import { Avatar, Card, Switch } from "antd";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { Card } from "antd";
+import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 import styles from "./style.module.scss";
 
-const { Meta } = Card;
+import PostCustomSkelton from "./postCard";
+import SubscriptionCustomSkelton from "./subscriptionCard";
 
-const CustomSkelton = ({ skeltonFor, bodyCount, stylingClass, isButton }) => {
+const CustomSkelton = ({
+  skeltonFor,
+  bodyCount,
+  stylingClass,
+  listStyle = "blank",
+  isButton,
+}) => {
   return skeltonFor == "card" ? (
     <Card
       className={styles.cardSkeleton}
       actions={[
         <div className={styles.cardSkeleton_actions}>
-          <Skeleton
-            className={styles.cardSkeleton_actions_button}
-            count={1}
-          />
+          <Skeleton className={styles.cardSkeleton_actions_button} count={1} />
         </div>,
       ]}
-      title=<Skeleton height={29} className={styles[stylingClass]} style={{margin: '2px 0'}} count={1} />
+      title={
+        <Skeleton
+          height={29}
+          className={styles[stylingClass]}
+          style={{ margin: "2px 0" }}
+          count={1}
+        />
+      }
     >
       <Skeleton className={styles[stylingClass]} count={bodyCount} />
     </Card>
   ) : skeltonFor == "list" ? (
-      <Skeleton className={styles.listSkeleton} count={bodyCount} />
+    <Skeleton
+      className={`${styles.listSkeleton} ${styles[listStyle]}`}
+      count={bodyCount}
+    />
   ) : skeltonFor == "tree" ? (
     <ul className={styles.treeSkeleton}>
       <li>
@@ -35,7 +49,7 @@ const CustomSkelton = ({ skeltonFor, bodyCount, stylingClass, isButton }) => {
               <li>
                 <Skeleton />
                 <ul>
-                <li>
+                  <li>
                     <Skeleton />
                   </li>
                   <li>
@@ -74,7 +88,11 @@ const CustomSkelton = ({ skeltonFor, bodyCount, stylingClass, isButton }) => {
         </ul>
       </li>
     </ul>
-) : null;
+  ) : skeltonFor == "post_card" ? (
+    <PostCustomSkelton bodyCount={bodyCount} stylingClass={stylingClass} />
+  ) : skeltonFor == "subscription_card" ? (
+    <SubscriptionCustomSkelton stylingClass={stylingClass} />
+  ) : null;
 };
 
 export default CustomSkelton;
