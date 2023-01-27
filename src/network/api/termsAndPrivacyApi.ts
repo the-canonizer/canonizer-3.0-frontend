1792e1865898afd643f77191451233756d7e8ec1
Rahul Kumar Singh <rahul.singh@iffort.com>
2023-01-27 13:18:52 +0530

    client token implementatin on privacy and term pages.

--- src/network/api/termsAndPrivacyApi.ts
+++ src/network/api/termsAndPrivacyApi.ts
@@ -1,22 +1,35 @@
 import { handleError } from "../../utils/generalUtility";
 import NetworkCall from "../networkCall";
 import TermsAndPrivacyRequest from "../request/termsAndPrivacyRequest";
+import { createToken } from "./userApi";
 
-export const getTermsAndServicesContent = async () => {
+export const getTermsAndServicesContent = async (tc = "") => {
+  let token = tc;
+  if (!tc) {
+    const response = await createToken();
+    token = response?.access_token;
+  }
   try {
     const res = await NetworkCall.fetch(
-      TermsAndPrivacyRequest.getTermsAndServicesContent()
+      TermsAndPrivacyRequest.getTermsAndServicesContent(token)
     );
+    console.log(res, "term and service");
     return res.data;
   } catch (error) {
     handleError(error);
   }
 };
-export const getPrivacyPolicyContent = async () => {
+export const getPrivacyPolicyContent = async (tc = "") => {
+  let token = tc;
+  if (!tc) {
+    const response = await createToken();
+    token = response?.access_token;
+  }
   try {
     const res = await NetworkCall.fetch(
-      TermsAndPrivacyRequest.getPrivacyPolicyContent()
+      TermsAndPrivacyRequest.getPrivacyPolicyContent(token)
     );
+    console.log(res, "privacy policy");
     return res.data;
   } catch (error) {
     handleError(error);
