c3262c8ebef6834373bceb0491cf662290af820e
vikasrana <vikas.rana@talentelgia.in>
2023-03-15 19:56:01 +0530

    Ticket #206
    Instead of showing "Camp" name it is showing "Topic" name.

--- src/components/ComponentPages/DirectSupportedCamps/index.tsx
+++ src/components/ComponentPages/DirectSupportedCamps/index.tsx
@@ -31,6 +31,9 @@ const DirectSupportedCamps = ({ search }: any) => {
   const [statusFlag, setStatusFlag] = useState(true);
   const [directSkeletonIndicator, setDirectSkeletonIndicator] = useState(false);
   const [modalPopupText, setModalPopupText] = useState(false);
+  const [removeCampLink, setRemoveCamplink] = useState([]);
+
+
 
   const handleSupportedCampsCancel = () => {
     setIsSupportedCampsModalVisible(false);
@@ -61,6 +64,7 @@ const DirectSupportedCamps = ({ search }: any) => {
       val.dis = false;
     });
     setcampIds([]);
+    setRemoveCamplink([])
     setRevertBack(camps);
   };
 
@@ -78,7 +82,7 @@ const DirectSupportedCamps = ({ search }: any) => {
     handleRevertBack(cardCamp_ID, data[0].camps);
     Object.keys(val).length === 0
       ? setcampIds([])
-      : ((val.dis = true), setcampIds([val.camp_num]));
+      : ((val.dis = true), setcampIds([val.camp_num]),setRemoveCamplink([val]));
     setIdData(topicId), setCardCamp_ID(topicId);
     setVisible(false);
   };
@@ -88,14 +92,14 @@ const DirectSupportedCamps = ({ search }: any) => {
     if (cardCamp_ID == "") {
       Object.keys(val).length === 0
         ? setcampIds([])
-        : ((val.dis = true), setcampIds([val.camp_num]));
+        : ((val.dis = true), setcampIds([val.camp_num]),setRemoveCamplink([val]));
       setShowSaveChanges(true);
       setCardCamp_ID(id);
       setIdData(id);
     } else if (cardCamp_ID && cardCamp_ID == id) {
       Object.keys(val).length === 0
         ? setcampIds([])
-        : ((val.dis = true), setcampIds([...campIds, val.camp_num]));
+        : ((val.dis = true), setcampIds([...campIds, val.camp_num]),setRemoveCamplink([...removeCampLink,val]));
       setShowSaveChanges(true);
       setCardCamp_ID(id);
     } else if (cardCamp_ID && cardCamp_ID != id) {
@@ -209,6 +213,8 @@ const DirectSupportedCamps = ({ search }: any) => {
       handleSupportedCampsOpen={handleSupportedCampsOpen}
       modalPopupText={modalPopupText}
       campIds={campIds}
+      CardData={CardData}
+      removeCampLink={removeCampLink}
     />
   );
 };
