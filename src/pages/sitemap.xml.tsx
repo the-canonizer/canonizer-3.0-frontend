0034d6a916ddd1ce3bcfdbd634230c70d1cd25f1
Rahul Kumar Singh <rahul.singh@iffort.com>
2023-03-22 18:10:20 +0530

    xml formatted.

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -15,11 +15,39 @@ export const getServerSideProps = async ({ res }) => {
 
   keys.forEach((key) => {
     if (key == "index") {
-      sitemap = `<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="sitemap-css/main-sitemap.xsl"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${data[key]?.map((d: { [x: string]: any; url: any }) =>`<sitemap><loc>${d.url}</loc><lastmod>${d.last_modified}</lastmod></sitemap>`).join("")}</sitemapindex>`;
+      sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <?xml-stylesheet type="text/xsl" href="sitemap-css/main-sitemap.xsl"?>
+      <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${data[key]?.map((d: { [x: string]: any; url: any }) =>`
+        <sitemap>
+          <loc>
+            ${d.url}
+          </loc>
+          <lastmod>
+            ${d.last_modified}
+          </lastmod>
+        </sitemap>
+        `).join("")}
+      </sitemapindex>
+      `;
       res.setHeader("Content-Type", "text/xml");
       res.write(sitemap);
     } else {
-      sitemap = `<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="sitemap-css/main-sitemap.xsl"?><urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${data[key]?.map((d: { [x: string]: any; url: any }) => `<url><loc>${d.url}</loc><lastmod>${d.last_modified}</lastmod></url>`).join("")}</urlset>`;
+      sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <?xml-stylesheet type="text/xsl" href="sitemap-css/main-sitemap.xsl"?>
+      <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${data[key]?.map((d: { [x: string]: any; url: any }) => `
+        <url>
+          <loc>
+            ${d.url}
+          </loc>
+          <lastmod>
+            ${d.last_modified}
+          </lastmod>
+        </url>
+        `).join("")}
+      </urlset>
+      `;
       fs.writeFileSync(`public/${key}`, sitemap, {encoding:'utf8',flag:'w'});
     }
   });
