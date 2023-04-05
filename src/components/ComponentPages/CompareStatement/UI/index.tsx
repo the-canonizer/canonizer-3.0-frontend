f9e2233f02c3e4077dedde75fedc9a88b73142a3
vikasrana <vikas.rana@talentelgia.in>
2023-04-05 11:37:24 +0530

    Ticket #862
    Comment out camp archive functionality

--- src/components/ComponentPages/CompareStatement/UI/index.tsx
+++ src/components/ComponentPages/CompareStatement/UI/index.tsx
@@ -180,6 +180,10 @@ function CompareStatementUI({
                           <Text strong>Single Level Camps Only : </Text>
                           <Text>{s1?.is_one_level == 1 ? "Yes" : "No"}</Text>
                         </Paragraph>
+                        {/* <Paragraph>
+                          <Text strong>Camp archive : </Text>
+                          <Text>{s1?.is_archive == 1 ? "Yes" : "No"}</Text>
+                        </Paragraph> */}
                       </Fragment>
                     ) : null}
                     <Text strong style={{ textTransform: "capitalize" }}>
@@ -297,6 +301,10 @@ function CompareStatementUI({
                           <Text strong>Single Level Camps Only : </Text>
                           <Text>{s2?.is_one_level == 1 ? "Yes" : "No"}</Text>
                         </Paragraph>
+                        {/* <Paragraph>
+                          <Text strong>Camp archive : </Text>
+                          <Text>{s2?.is_archive == 1 ? "Yes" : "No"}</Text>
+                        </Paragraph> */}
                       </Fragment>
                     ) : null}
                     <Text strong style={{ textTransform: "capitalize" }}>
@@ -446,6 +454,12 @@ function CompareStatementUI({
                             {liveStatement?.is_one_level == 1 ? "Yes" : "No"}
                           </Text>
                         </Paragraph>
+                        {/* <Paragraph>
+                          <Text strong>Camp archive: </Text>
+                          <Text>
+                            {liveStatement?.is_archive == 1 ? "Yes" : "No"}
+                          </Text>
+                        </Paragraph> */}
                       </Fragment>
                     ) : null}
                   </Card>
