95159b4acb2ee10fef8a7d91fa6a2e805b36d377
Rahul Kumar Singh <rahul.singh@iffort.com>
2023-04-05 14:57:01 +0530

    add meta api in server function for fix seo issue for metas

--- src/components/common/headContentAndPermisisonCheck/index.tsx
+++ src/components/common/headContentAndPermisisonCheck/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 
@@ -6,16 +6,17 @@ import HeadContent from "./headContent";
 import PermissionsForPages from "../../../permissions";
 import usePermission from "../../../hooks/usePermissions";
 import useAuthentication from "../../../hooks/isUserAuthenticated";
-import { metaTagsApi } from "src/network/api/metaTagsAPI";
 import { RootState } from "src/store";
 import { createToken } from "src/network/api/userApi";
 
 type HeadContentComponentProps = {
   componentName: string;
+  metaContent: any;
 };
 
 const HeadContentAndPermissionComponent = ({
   componentName,
+  metaContent,
 }: HeadContentComponentProps) => {
   const router = useRouter();
   const pageRoute = process.env.NEXT_PUBLIC_BASE_URL + router?.asPath;
@@ -26,7 +27,6 @@ const HeadContentAndPermissionComponent = ({
 
   const { isAllowed } = usePermission();
   const { isUserAuthenticated } = useAuthentication();
-  const [metaContent, setMetaContent] = useState(null);
 
   const getToken = async () => {
     if (!authToken) await createToken();
@@ -65,49 +65,13 @@ const HeadContentAndPermissionComponent = ({
     }
   }, [componentName, isUserAuthenticated, isAllowed, router]);
 
-  useEffect(() => {
-    const req = {
-      page_name:
-        componentName == "SocialLoginCallbackPage" ? "Home" : componentName,
-      keys: {
-        topic_num: router.asPath.includes("forum")
-          ? router?.query?.topic?.toLocaleString().split("-")[0]
-          : router?.query?.camp?.length && router.query.camp[0].split("-")[0],
-        camp_num: router.asPath.includes("forum")
-          ? router.query.camp?.toLocaleString().split("-")[0]
-          : router?.query?.camp?.length > 1
-          ? router.query.camp[1].split("-")[0]
-          : "1",
-        forum_num:
-          router?.query?.camp?.length > 2
-            ? Object.keys(router.query)?.length > 2
-              ? router?.query?.id
-              : null
-            : null,
-      },
-    };
-    const defaultTags = {
-      page_name: "Home",
-      title: "Build consensus by canonizing what you believe is right",
-      description:
-        "Bringing the world together by canonizing what you believe is right. Your thoughts are processed through our pattented algorithims in a qualified & quantified camp where others can see, join & together change the world.",
-      author: "",
-    };
-    async function apiCall() {
-      const result = await metaTagsApi(req);
-      result?.status_code == 200
-        ? setMetaContent(result?.data)
-        : setMetaContent(defaultTags);
-    }
-    apiCall();
-  }, [componentName, router?.query]);
-
   return (
     <HeadContent
       title={metaContent?.title}
       description={metaContent?.description}
       route={pageRoute}
       author={metaContent?.author}
+      componentName={componentName}
     />
   );
 };
