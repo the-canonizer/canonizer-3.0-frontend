1a958d414638c3eacebd9d3c4f74b1e9973f62fd
vikasrana <vikas.rana@talentelgia.in>
2023-04-26 18:10:04 +0530

    Ticket #965
    Frontend: Test cases for Archiving and Unarchiving camp

---
+++ src/components/ComponentPages/ArchivedCampCheckBox/archivedCampCheckBox.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "../../../utils/testUtils";
+
+
+import ArchivedCampCheckBox from ".";
+
+describe("Compare Statement page", () => {
+    it("should render without crash", () => {
+      render(<ArchivedCampCheckBox />);
+  
+      expect(
+        screen.getByText("Show archived camps")
+      ).toBeInTheDocument();
+      
+    });
+  });
--- src/components/ComponentPages/CompareStatement/__test__/CompareStatement.test.tsx
+++ src/components/ComponentPages/CompareStatement/__test__/CompareStatement.test.tsx
@@ -1,6 +1,12 @@
 import { render, screen } from "../../../../utils/testUtils";
 
 import Statements from "../";
+import CompareStatementUI from "../";
+
+const statements = []
+const isLoading  = false
+const liveStatement = {}
+const itemsStatus   = {}
 
 describe("Compare Statement page", () => {
   it("should render without crash", () => {
@@ -11,5 +17,20 @@ describe("Compare Statement page", () => {
     ).toBeInTheDocument();
     expect(screen.getByText("Create New Topic")).toBeInTheDocument();
     expect(screen.getByText("Create New Camp")).toBeInTheDocument();
+  });
+});
+
+describe("Compare Statement page", () => {
+  it("should render without crash", () => {
+    const {container}=render(<CompareStatementUI
+      statements={statements}
+      isLoading={isLoading}
+      liveStatement={liveStatement}
+      itemsStatus={itemsStatus}
+      />);
+  expect( container.getElementsByClassName("ant-typography")
+    ).toBeTruthy()
+  expect(screen.queryByTestId("Camp archive:")).toBeNull();
+    
   });
 });
