5cab94cbfb17dfa7cfeb15247d71068591333f08
shahab-ramzan <shahab.ramzan@codedistrict.com>
2023-05-31 14:05:15 +0500

    tests added for topics list

--- src/components/ComponentPages/Home/TopicsList/__test__/topicsList.test.tsx
+++ src/components/ComponentPages/Home/TopicsList/__test__/topicsList.test.tsx
@@ -1,60 +1,83 @@
-// import { render, screen, cleanup } from "@testing-library/react";
-// import HomePageContainer from "../../index";
-// import { Provider } from "react-redux";
-// import { store } from "src/store";
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { useSelector, useDispatch } from "react-redux";
+import TopicsList from "../index";
 
-// jest.mock("../../../../../hooks/isUserAuthenticated", () => ({
-//   __esModule: true,
-//   default: jest.fn(() => ({ isUserAuthenticated: true })),
-// }));
+jest.mock("react-redux"); // Mock react-redux hooks
 
-// window.matchMedia =
-//   window.matchMedia ||
-//   function () {
-//     return {
-//       matches: false,
-//       addListener: function () {},
-//       removeListener: function () {},
-//     };
-//   };
+describe("TopicsList", () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) =>
+      selector({
+        homePage: {
+          canonizedTopicsData: {
+            topics: [
+              { topic_id: 1, topic_name: "Topic 1", topic_score: 5 },
+              { topic_id: 2, topic_name: "Topic 2", topic_score: 7 },
+            ],
+            numOfPages: 2,
+          },
+          nameSpaces: [
+            { id: 1, label: "/general/" },
+            { id: 2, label: "/organization/" },
+          ],
+        },
+        filters: {
+          filterObject: {
+            asofdate: "",
+            asof: "",
+            algorithm: "",
+            filterByScore: "",
+            nameSpace: "",
+            namespace_id: "",
+            search: "",
+            is_archive: "",
+          },
+        },
+        auth: {
+          loggedInUser: { email: "test@example.com" },
+        },
+        utils: {
+          score_checkbox: true,
+          archived_checkbox: false,
+        },
+      })
+    );
 
-// afterEach(cleanup);
+    useDispatch.mockReturnValue(jest.fn());
+  });
 
-// describe("HomePageContainer", () => {
-//   test("renders SideBar component", () => {
-//     render(<Provider store={store}><HomePageContainer /></Provider>);
-//     const sideBarElement = screen.getByTestId("sideBar");
-//     expect(sideBarElement).toBeInTheDocument();
-//   });
+  afterEach(() => {
+    useSelector.mockClear();
+    useDispatch.mockClear();
+  });
 
-//   test("renders TopicsList component", () => {
-//     render(<Provider store={store}><HomePageContainer /></Provider>);
-//     const topicsListElement = screen.getByTestId("topicsList");
-//     expect(topicsListElement).toBeInTheDocument();
-//   });
+  test("renders the topic list correctly", () => {
+    render(<TopicsList />);
 
-//   test("renders RecentActivities component when user is authenticated", () => {
-//     render(<Provider store={store}><HomePageContainer /></Provider>);
-//     const recentActivitiesElement = screen.getByTestId("recentActivities");
-//     expect(recentActivitiesElement).toBeInTheDocument();
-//   });
+    expect(screen.getByText("Topic 1")).toBeInTheDocument();
+    expect(screen.getByText("Topic 2")).toBeInTheDocument();
+  });
 
-//   test("does not render RecentActivities component when user is not authenticated", () => {
-//     jest.mock("../../../../../hooks/isUserAuthenticated", () => ({
-//       __esModule: true,
-//       default: jest.fn(() => ({ isUserAuthenticated: false })),
-//     }));
+  test("loads more topics when 'Load More' button is clicked", () => {
+    render(<TopicsList />);
 
-//     render(<Provider store={store}><HomePageContainer /></Provider>);
-//     const recentActivitiesElement = screen.queryByTestId("recentActivities");
-//     expect(recentActivitiesElement).toBeNull();
-//   });
+    fireEvent.click(screen.getByText("Load More"));
 
-//   test("renders HelpCard component", () => {
-//     render(<Provider store={store}> <HomePageContainer /></Provider>);
-//     const helpCardElement = screen.getByTestId("helpCard");
-//     expect(helpCardElement).toBeInTheDocument();
-//   });
+    // Add assertions for the expected behavior when 'Load More' button is clicked
+  });
 
-  
-// });
+  test("updates the search input value correctly", () => {
+    render(<TopicsList />);
+
+    fireEvent.change(screen.getByPlaceholderText("Search by topic name"), {
+      target: { value: "Topic 3" },
+    });
+
+    expect(screen.getByPlaceholderText("Search by topic name").value).toBe(
+      "Topic 3"
+    );
+  });
+
+ 
+});
