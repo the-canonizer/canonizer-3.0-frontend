167f3e3bb249be116a899f1781052fb33d9f67c6
Rahul Kumar Singh <rahul.singh@iffort.com>
2023-09-19 22:05:32 +0530

    ticket #1279

--- src/components/ComponentPages/Home/SideBar/index.tsx
+++ src/components/ComponentPages/Home/SideBar/index.tsx
@@ -2,7 +2,7 @@ import { useState, Fragment, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Drawer } from "antd";
-import { CloseCircleOutlined } from "@ant-design/icons";
+// import { CloseCircleOutlined } from "@ant-design/icons";
 
 import { RootState } from "src/store";
 import TopicsFilter from "../../../common/topicsFilter";
@@ -28,11 +28,19 @@ export default function HomeSideBar({
   );
 
   const [drawerIsVisible, setDrawerIsVisible] = useState(drawerShow);
+  const [isDrawerOpen, setIsDrawerOpen] = useState("");
 
   const router = useRouter();
   const dispatch = useDispatch();
 
-  useEffect(() => setDrawerIsVisible(drawerShow), [drawerShow]);
+  useEffect(() => {
+    if (drawerShow) {
+      setIsDrawerOpen("isDrawerOpen");
+    } else {
+      setIsDrawerOpen("");
+    }
+    setDrawerIsVisible(drawerShow);
+  }, [drawerShow]);
 
   const showDrawer = () => {
     router.push(
@@ -44,47 +52,49 @@ export default function HomeSideBar({
           : ""
       }&asof=${filterObject?.asof}&canon=${
         filterObject?.namespace_id
-      }&is_tree_open=1${viewThisVersion ? "&viewversion=1" : ""}`,
+      }&is_tree_open=${drawerIsVisible ? 0 : 1}${
+        viewThisVersion ? "&viewversion=1" : ""
+      }`,
       null,
       {
         shallow: true,
       }
     );
 
-    dispatch(setShowDrawer(true));
+    dispatch(setShowDrawer(!drawerIsVisible));
   };
 
-  const onClose = () => {
-    router.push(
-      `/topic/${router?.query?.camp[0]}/${
-        router?.query?.camp[1]
-      }?score=${filterByScore}&algo=${filterObject?.algorithm}${
-        filterObject?.asof == "bydate"
-          ? "&asofdate=" + filterObject?.asofdate
-          : ""
-      }&asof=${filterObject?.asof}&canon=${
-        filterObject?.namespace_id
-      }&is_tree_open=0${viewThisVersion ? "&viewversion=1" : ""}`,
-      null,
-      {
-        shallow: true,
-      }
-    );
+  // const onClose = () => {
+  //   router.push(
+  //     `/topic/${router?.query?.camp[0]}/${
+  //       router?.query?.camp[1]
+  //     }?score=${filterByScore}&algo=${filterObject?.algorithm}${
+  //       filterObject?.asof == "bydate"
+  //         ? "&asofdate=" + filterObject?.asofdate
+  //         : ""
+  //     }&asof=${filterObject?.asof}&canon=${
+  //       filterObject?.namespace_id
+  //     }&is_tree_open=0${viewThisVersion ? "&viewversion=1" : ""}`,
+  //     null,
+  //     {
+  //       shallow: true,
+  //     }
+  //   );
 
-    dispatch(setShowDrawer(false));
-  };
+  //   dispatch(setShowDrawer(false));
+  // };
 
   return (
     <Fragment>
-      {" "}
       {!router?.asPath?.includes("topic") ? (
-        <TopicsFilter />
+        <TopicsFilter key="topic_filter" />
       ) : (
         <Fragment>
           <Button
+            key="tree-opener-btn"
             type="primary"
             onClick={showDrawer}
-            className="btnFilter drawerBtn"
+            className={`btnFilter drawerBtn ${isDrawerOpen}`}
           >
             Consensus Tree{" "}
             <p className="arrow">
@@ -96,10 +106,11 @@ export default function HomeSideBar({
           <Drawer
             title="Consensus Tree"
             placement="left"
-            onClose={onClose}
+            // onClose={onClose}
             visible={drawerIsVisible}
             className={`treeDrawer ${backGroundColorClass}`}
-            closeIcon={<CloseCircleOutlined />}
+            // closeIcon={<CloseCircleOutlined />}
+            closeIcon={null}
             height={"auto"}
             size="large"
             bodyStyle={{ paddingBottom: 80 }}
