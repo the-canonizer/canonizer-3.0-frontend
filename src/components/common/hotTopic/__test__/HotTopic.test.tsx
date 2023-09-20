26fa05a02c836edf373b10d33b635d44df68a063
Rahul Kumar Singh <rahul.singh@iffort.com>
2023-09-20 10:20:16 +0530

    test case corection.

--- src/components/common/hotTopic/__test__/HotTopic.test.tsx
+++ src/components/common/hotTopic/__test__/HotTopic.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, cleanup, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "src/store";
+import configureMockStore from "redux-mock-store";
 
 import HotTopic from "../";
 
@@ -15,12 +15,60 @@ window.matchMedia =
     };
   };
 
+const mockStore = configureMockStore();
+const store1 = mockStore({
+  auth: {
+    authenticated: true,
+    loggedInUser: {
+      is_admin: true,
+    },
+  },
+  topicDetails: {
+    currentCampRecord: { parentCamps: [{ camp_name: "camp one" }] },
+  },
+  filters: {
+    filterObject: {},
+  },
+  forum: {
+    currentThread: null,
+    currentPost: null,
+  },
+  hotTopic: {
+    topicData: {
+      id: 1,
+      updated_at: Date.now(),
+      created_at: Date.now(),
+      file_full_path: "",
+      topic_num: 88,
+      topic_name: "Theories",
+      camp_num: "",
+      camp_name: "Agreement",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo animi distinctio sed? Sapiente sed numquam molestiae deserunt neque temporibus officiis qui, doloremque excepturi rem harum nihil iste. Magnam ut veniam nobis corporis a amet reprehenderit officia enim fugiat sit, dignissimos cupiditate eius, dolores libero sequi aspernatur exercitationem in necessitatibus quasi!",
+    },
+  },
+});
+
 afterEach(cleanup);
 
 // Mock the API functions used in the component
 jest.mock("src/network/api/topicAPI", () => ({
   GetHotTopicDetails: jest.fn(() =>
-    Promise.resolve({ status_code: 200, data: [] })
+    Promise.resolve({
+      status_code: 200,
+      data: {
+        id: 1,
+        updated_at: Date.now(),
+        created_at: Date.now(),
+        file_full_path: "",
+        topic_num: 88,
+        topic_name: "Theories",
+        camp_num: "",
+        camp_name: "Agreement",
+        description:
+          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo animi distinctio sed? Sapiente sed numquam molestiae deserunt neque temporibus officiis qui, doloremque excepturi rem harum nihil iste. Magnam ut veniam nobis corporis a amet reprehenderit officia enim fugiat sit, dignissimos cupiditate eius, dolores libero sequi aspernatur exercitationem in necessitatibus quasi!",
+      },
+    })
   ),
 }));
 
@@ -29,18 +77,35 @@ describe("AddOrManage component", () => {
     jest.clearAllMocks();
     jest.mock("src/network/api/topicAPI", () => ({
       GetHotTopicDetails: jest.fn(() =>
-        Promise.resolve({ status_code: 200, data: [] })
+        Promise.resolve({
+          status_code: 200,
+          data: {
+            title: "Hot Topic",
+            id: 1,
+            updated_at: Date.now(),
+            created_at: Date.now(),
+            file_full_path: "",
+            topic_num: 88,
+            topic_name: "Theories",
+            camp_num: "",
+            camp_name: "Agreement",
+            description:
+              "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo animi distinctio sed? Sapiente sed numquam molestiae deserunt neque temporibus officiis qui, doloremque excepturi rem harum nihil iste. Magnam ut veniam nobis corporis a amet reprehenderit officia enim fugiat sit, dignissimos cupiditate eius, dolores libero sequi aspernatur exercitationem in necessitatibus quasi!",
+          },
+        })
       ),
     }));
   });
 
   test("Render Hot topic", () => {
     render(
-      <Provider store={store}>
+      <Provider store={store1}>
         <HotTopic />
       </Provider>
     );
 
     expect(screen.getByText("Hot Topic")).toBeInTheDocument();
+    expect(screen.getByText("Theories")).toBeInTheDocument();
+    expect(screen.getByText("Agreement")).toBeInTheDocument();
   });
 });
