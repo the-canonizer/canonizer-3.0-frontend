cee63723c257073962c0a73b8184c578a60ae193
Rahul Kumar Singh <rahul.singh@iffort.com>
2023-10-18 10:24:34 +0530

    video test case updated.

--- src/components/ComponentPages/canonVideos/__test__/canonVideos.test.tsx
+++ src/components/ComponentPages/canonVideos/__test__/canonVideos.test.tsx
@@ -13,7 +13,6 @@ import configureMockStore from "redux-mock-store";
 
 import CanonVideos from "../";
 import { store } from "src/store";
-// import { act } from "react-dom/test-utils";
 import { getVideosContentApi } from "src/network/api/videos";
 
 jest.mock("next/router", () => ({
@@ -114,12 +113,6 @@ const resDat = [
   },
 ];
 
-jest.mock("src/network/api/videos", () => ({
-  getVideosContentApi: jest.fn(() =>
-    Promise.resolve({ status_code: 200, data: resDat })
-  ),
-}));
-
 jest.mock("src/network/api/videos");
 
 const mockStore = configureMockStore();
@@ -146,38 +139,9 @@ afterEach(cleanup);
 
 describe("Canonizer Videos", () => {
   beforeEach(() => {
-    jest.mock("src/network/api/videos", () => ({
-      getVideosContentApi: jest.fn(() =>
-        Promise.resolve({
-          status_code: 200,
-          data: resDat,
-        })
-      ),
-    }));
     jest.mock("src/network/api/videos");
   });
 
-  it("renders video titles", () => {
-    render(
-      <Provider store={store}>
-        <RouterContext.Provider
-          value={createMockRouter({
-            route: "/videos/consciousness",
-            asPath: "/videos/consciousness/introduction",
-          })}
-        >
-          <CanonVideos />
-        </RouterContext.Provider>
-      </Provider>
-    );
-    expect(
-      screen.getByText(
-        "Consciousness: Not a Hard Problem, Just a Color Problem"
-      )
-    ).toBeInTheDocument();
-  });
-
-  /* eslint-disable */
   it("changes video when clicked on video title", () => {
     render(
       <Provider store={store}>
@@ -193,30 +157,8 @@ describe("Canonizer Videos", () => {
     );
     const videoTitle = screen.getByText("Video Format:");
     fireEvent.click(videoTitle);
-  });
-  /* eslint-enable */
-
-  it("renders video player with controls when video is available", async () => {
-    render(<CanonVideos />);
-
-    await waitFor(() => {
-      const videoPlayer = screen.getByTestId("videoPlayer");
-      expect(videoPlayer).toBeInTheDocument();
-    });
-  });
-
-  it("render heading and labels", () => {
-    render(
-      <Fragment>
-        <Provider store={store}>
-          <RouterContext.Provider
-            value={createMockRouter({ asPath: "/videos/consciousness" })}
-          >
-            <CanonVideos />
-          </RouterContext.Provider>
-        </Provider>
-      </Fragment>
-    );
+    const videoPlayer = screen.getByTestId("videoPlayer");
+    expect(videoPlayer).toBeInTheDocument();
 
     const mainHeadig = screen.getByRole("heading", {
       name: /Consciousness: Not a Hard Problem, Just a Color Problem/i,
@@ -226,7 +168,8 @@ describe("Canonizer Videos", () => {
     expect(mainHeadig).toBeInTheDocument();
     expect(topictab).toBeInTheDocument();
   });
-  it("render", async () => {
+
+  it("test list", async () => {
     getVideosContentApi.mockResolvedValue({
       status_code: 200,
       data: resDat,
@@ -249,22 +192,18 @@ describe("Canonizer Videos", () => {
 
     await waitFor(() => {
       expect(getVideosContentApi).toHaveBeenCalled();
-      expect(screen.getByText("Introduction")).toBeInTheDocument();
-      expect(screen.getByText("Perceiving a Strawberry")).toBeInTheDocument();
-      fireEvent.click(screen.getByText("Perceiving a Strawberry"));
-      expect(screen.getByTestId("playerId")).toHaveAttribute(
-        "src",
-        "https://canonizer3.canonizer.com/static/videos/consciousness/perceiving_a_strawberry_360.mp4"
-      );
-      // fireEvent.click(screen.getByText("perceiving_a_strawberry_1080.mp4"));
-      // expect(screen.getByTestId("playerId")).toHaveAttribute(
-      //   "src",
-      //   "https://canonizer3.canonizer.com/static/videos/consciousness/perceiving_a_strawberry_1080.mp4"
-      // );
-
-      expect(screen.getByText("360 P")).toBeInTheDocument();
-      expect(screen.getByText("720 P")).toBeInTheDocument();
-      expect(screen.getByText("1080 P")).toBeInTheDocument();
     });
+
+    expect(screen.getByText("Introduction")).toBeInTheDocument();
+    expect(screen.getByText("Perceiving a Strawberry")).toBeInTheDocument();
+    fireEvent.click(screen.getByText("Perceiving a Strawberry"));
+    expect(screen.getByTestId("playerId")).toHaveAttribute(
+      "src",
+      "https://canonizer3.canonizer.com/static/videos/consciousness/perceiving_a_strawberry_360.mp4"
+    );
+
+    expect(screen.getByText("360 P")).toBeInTheDocument();
+    expect(screen.getByText("720 P")).toBeInTheDocument();
+    expect(screen.getByText("1080 P")).toBeInTheDocument();
   });
 });
