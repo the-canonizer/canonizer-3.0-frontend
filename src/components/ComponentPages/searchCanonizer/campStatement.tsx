4d20f881d8083fa93da40f6701cc0aa4029d6480
vikasrana <vikas.rana@talentelgia.in>
2023-11-23 01:02:22 +0530

    Ticket #629 #632 #634 #635
    Global search- Show message" No data found" when searching for random keyword
    Global search- show hand pointer on the searched keyword that are displayed
    Global search- UI issue- > Font style must be uniform in search results in the droop down for topi/statement /camp
    Global search- When try to search any result quickly, I see random result for a sec then I see the correct result

--- src/components/ComponentPages/searchCanonizer/campStatement.tsx
+++ src/components/ComponentPages/searchCanonizer/campStatement.tsx
@@ -5,7 +5,7 @@ import styles from "./search.module.scss";
 import { useSelector } from "react-redux";
 import { RootState } from "src/store";
 import moment from "moment";
-import { Pagination } from "antd";
+import { Empty, Pagination } from "antd";
 
 const CampStatementSearch = () => {
   const { searchData } = useSelector((state: RootState) => ({
@@ -28,7 +28,9 @@ const CampStatementSearch = () => {
     return moment(unixTime * 1000).format("DD MMMM YYYY, hh:mm:ss A");
   };
   const fileNameLength = 800;
-
+  const showEmpty = (msg) => {
+    return <Empty description={msg} />;
+  };
   return (
     <Fragment>
       <aside className="leftSideBar miniSideBar">
@@ -43,70 +45,80 @@ const CampStatementSearch = () => {
             {/* <AdvanceFilter /> */}
           </div>
           <div className={styles.search_lists}>
-            <ul>
-              {searchData?.statement
-                ?.slice(startingPosition, endingPosition)
-                .map((x) => {
-                  const jsonData = JSON.parse(x.breadcrumb_data) as Array<any>;
-                  const parsedData = jsonData.reduce(
-                    (accumulator, currentVal, index) => {
-                      const accIndex = index + 1;
-                      accumulator[index] = {
-                        camp_name:
-                          currentVal[accIndex]?.camp_name == "Agreement"
-                            ? currentVal[accIndex]?.topic_name
-                            : currentVal[accIndex]?.camp_name,
-                        camp_link: currentVal[accIndex]?.camp_link,
-                        topic_name: currentVal[accIndex]?.topic_name,
-                      };
-                      return accumulator;
-                    },
-                    []
-                  );
-                  return (
-                    <>
-                      <li>
-                        <a href={`/${jsonData[0][1].camp_link}`}>
-                          <h3 className={styles.statement_heading}>
-                            {jsonData.length > 1
-                              ? jsonData[0][1].camp_name
-                              : jsonData[0][1].topic_name}
-                          </h3>
-                        </a>
-                        <div className={styles.statement_date}>
-                          <strong>Go live Time : </strong>
-                          {covertToTime(x.go_live_time)}
-                        </div>
-                        <div className="d-flex flex-wrap w-100 mb-1">
-                          {/* <a className={styles.search_heading}>  */}
-                          <div
-                            dangerouslySetInnerHTML={{
-                              __html:
-                                x.type_value.substring(0, fileNameLength) +
-                                "...",
-                            }}
-                          ></div>
-                        </div>
-                        <div className={styles.tags_all}>
-                          {parsedData.reverse().map((obj, index) => {
-                            return (
-                              <>
-                                <a
-                                  href={`/${obj.camp_link}`}
-                                  key={`/${obj.camp_link}`}
-                                >
-                                  {obj.camp_name}
-                                  {index < parsedData.length - 1 ? "/ " : ""}
-                                </a>
-                              </>
-                            );
-                          })}
-                        </div>
-                      </li>
-                    </>
-                  );
-                })}
-            </ul>
+            {searchData.statement.length ? (
+              <div>
+                <ul>
+                  {searchData?.statement
+                    ?.slice(startingPosition, endingPosition)
+                    .map((x) => {
+                      const jsonData = JSON.parse(
+                        x.breadcrumb_data
+                      ) as Array<any>;
+                      const parsedData = jsonData.reduce(
+                        (accumulator, currentVal, index) => {
+                          const accIndex = index + 1;
+                          accumulator[index] = {
+                            camp_name:
+                              currentVal[accIndex]?.camp_name == "Agreement"
+                                ? currentVal[accIndex]?.topic_name
+                                : currentVal[accIndex]?.camp_name,
+                            camp_link: currentVal[accIndex]?.camp_link,
+                            topic_name: currentVal[accIndex]?.topic_name,
+                          };
+                          return accumulator;
+                        },
+                        []
+                      );
+                      return (
+                        <>
+                          <li>
+                            <a href={`/${jsonData[0][1].camp_link}`}>
+                              <h3 className={styles.statement_heading}>
+                                {jsonData.length > 1
+                                  ? jsonData[0][1].camp_name
+                                  : jsonData[0][1].topic_name}
+                              </h3>
+                            </a>
+                            <div className={styles.statement_date}>
+                              <strong>Go live Time : </strong>
+                              {covertToTime(x.go_live_time)}
+                            </div>
+                            <div className="d-flex flex-wrap w-100 mb-1">
+                              {/* <a className={styles.search_heading}>  */}
+                              <div
+                                dangerouslySetInnerHTML={{
+                                  __html:
+                                    x.type_value.substring(0, fileNameLength) +
+                                    "...",
+                                }}
+                              ></div>
+                            </div>
+                            <div className={styles.tags_all}>
+                              {parsedData.reverse().map((obj, index) => {
+                                return (
+                                  <>
+                                    <a
+                                      href={`/${obj.camp_link}`}
+                                      key={`/${obj.camp_link}`}
+                                    >
+                                      {obj.camp_name}
+                                      {index < parsedData.length - 1
+                                        ? "/ "
+                                        : ""}
+                                    </a>
+                                  </>
+                                );
+                              })}
+                            </div>
+                          </li>
+                        </>
+                      );
+                    })}
+                </ul>
+              </div>
+            ) : (
+              showEmpty("No Data Found")
+            )}
           </div>
           <Pagination
             hideOnSinglePage={true}
