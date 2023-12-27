4b3dea6ff592d3861a48e69024423fc9ca0d6c79
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2023-12-27 20:56:55 +0500

    fix url issue

--- src/components/ComponentPages/DataNotFound/dataNotFound.tsx
+++ src/components/ComponentPages/DataNotFound/dataNotFound.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import styles from "./datanotfound.module.scss";
-
-const DataNotFound = ({ name, backURL, message = "" }: any) => {
+import { Typography } from "antd";
+const { Link: AntLink } = Typography;
+const DataNotFound = ({ name, backURL, message = "", goBack = false }: any) => {
+  const router = useRouter();
   return (
     <>
       <div className={styles.errorPageContentWrap}>
@@ -20,9 +23,15 @@ const DataNotFound = ({ name, backURL, message = "" }: any) => {
         <div className={styles.errorPageContent}>
           <h3>{message ? message : name + "  Not Found"}</h3>
           <p>{`We're sorry, the page you requested could not be found.`}</p>
-          <Link href={{ pathname: backURL }}>
-            <a className={styles.btnGoBack}>Go Back</a>
-          </Link>
+          {goBack ? (
+            <AntLink onClick={() => router?.back()}>
+              <a className={styles.btnGoBack}>Go Back</a>
+            </AntLink>
+          ) : (
+            <Link href={{ pathname: backURL }}>
+              <a className={styles.btnGoBack}>Go Back</a>
+            </Link>
+          )}
         </div>
       </div>
     </>
