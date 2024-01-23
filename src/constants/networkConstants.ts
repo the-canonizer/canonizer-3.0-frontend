c06bd08d286e9034384a0e2b559ecfd1417eb05c
vikasrana <vikas.rana@talentelgia.in>
2024-01-23 15:40:15 +0530

    change the URL from network constant file
    change the URL from network constant file

--- src/constants/networkConstants.ts
+++ src/constants/networkConstants.ts
@@ -2,7 +2,6 @@ const BaseCanonizerServiceUrl = process.env.NEXT_PUBLIC_BASE_SERVICE_URL;
 const BaseCanonizerApiUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
 const BaseImagesURL = process.env.NEXT_PUBLIC_BASE_IMAGES_URL;
 const BaseVideosURL = process.env.NEXT_PUBLIC_BASE_VIDEOS_URL;
-// const BaseCanonizerDevApiUrl = process.env.NEXT_PUBLIC_BASE_API_DEV_URL;
 const BaseCanonizerDevApiUrl = process.env.NEXT_PUBLIC_BASE_API_DEV_URL;
 const NetworkConstants = {
   URL: {
@@ -94,7 +93,7 @@ const NetworkConstants = {
     //GlobalSearchUploadedFile
     GlobalSearchUploadedFile: `${BaseCanonizerApiUrl}/global-search-uploaded-files`,
     //Canonizer Global Search
-    canonizerGlobalSearch: `${BaseCanonizerDevApiUrl}/search`,
+    canonizerGlobalSearch: `${BaseCanonizerApiUrl}/search`,
     //AllSupportedCampsList
     AllSupportedCampsList: `${BaseCanonizerApiUrl}/user/supports/`,
 
