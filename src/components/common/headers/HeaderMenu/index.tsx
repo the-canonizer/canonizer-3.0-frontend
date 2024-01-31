75fd8f00747dc2329ca851619b095ded8614c37f
vikasrana <vikas.rana@talentelgia.in>
2024-01-31 14:25:22 +0530

    Ticket #571
    Remove API calls from Authentication Pages accessed by not logged in users

--- src/components/common/headers/HeaderMenu/index.tsx
+++ src/components/common/headers/HeaderMenu/index.tsx
@@ -514,7 +514,7 @@ const HeaderMenu = ({ loggedUser }: any) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loggedUser]);
   useEffect(() => {
-    if (inputSearch || searchValue) {
+    if ((inputSearch || searchValue) && router.pathname.includes('/search')) {
       getGlobalSearchCanonizerNav(searchValue, false);
     }
   }, [pageNumber, router.pathname]);
