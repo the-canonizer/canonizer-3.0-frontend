c0a23aadb50eae8e8b15b3c20e65be4df9be6950
Rahul Kumar Singh <rahul.singh@iffort.com>
2024-02-05 11:29:38 +0530

    ticket #923 fixd.

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -6,13 +6,11 @@ import { Row, Col } from "antd";
 import Layout from "../hoc/layout";
 import SideBar from "../components/ComponentPages/Home/SideBar";
 import TopicsList from "../components/ComponentPages/Home/TopicsList";
-import { setCurrentDate } from "src/store/slices/filtersSlice";
+import { setCurrentDate, setOnlyMyTopic } from "src/store/slices/filtersSlice";
 
 const BrowsePage = ({ current_date }: any) => {
   const dispatch = useDispatch();
   const router = useRouter();
-  // dispatch(setCanonizedNameSpaces(nameSpacesList));
-  // dispatch(setCanonizedAlgorithms(algorithms));
 
   dispatch(setCurrentDate(current_date));
 
@@ -25,24 +23,24 @@ const BrowsePage = ({ current_date }: any) => {
       router?.replace(router, null, { shallow: true });
     }
 
+    return () => dispatch(setOnlyMyTopic(false));
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <>
-      <Layout routeName={"browse"}>
-        <aside className="leftSideBar miniSideBar">
-          <SideBar />
-        </aside>
-        <div className="pageContentWrap">
-          <Row gutter={16}>
-            <Col xs={24} sm={24} xl={24}>
-              <TopicsList />
-            </Col>
-          </Row>
-        </div>
-      </Layout>
-    </>
+    <Layout routeName={"browse"}>
+      <aside className="leftSideBar miniSideBar">
+        <SideBar />
+      </aside>
+      <div className="pageContentWrap">
+        <Row gutter={16}>
+          <Col xs={24} sm={24} xl={24}>
+            <TopicsList />
+          </Col>
+        </Row>
+      </div>
+    </Layout>
   );
 };
 
@@ -55,20 +53,6 @@ export async function getServerSideProps() {
     },
   };
 }
-
-// export async function getServerSideProps() {
-//   const [nameSpaces, canonizedAlgorithms] = await Promise.all([
-//     getCanonizedNameSpacesApi(),
-//     getCanonizedAlgorithmsApi(),
-//   ]);
-
-//   return {
-//     props: {
-//       nameSpacesList: nameSpaces || [],
-//       algorithms: canonizedAlgorithms || [],
-//     },
-//   };
-// }
 
 BrowsePage.displayName = "BrowsePage";
 
