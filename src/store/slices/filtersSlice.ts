7d0fa5f94e05e7b8b794f193b89df45cf776134f
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-02-26 11:51:14 +0500

    build issue fixed

--- src/store/slices/filtersSlice.ts
+++ src/store/slices/filtersSlice.ts
@@ -7,7 +7,7 @@ export const filtersSlice = createSlice({
       page_number: 1,
       page_size: 15,
       nameSpace: "/General/",
-      namespace_id: "1",
+      namespace_id: 1,
       asofdate: Date.now() / 1000,
       asof: "default",
       filterByScore: 0,
