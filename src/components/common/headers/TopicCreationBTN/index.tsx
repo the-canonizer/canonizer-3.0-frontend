a109878028aaa443aad7f4ef53100be5e108932d
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-03-13 10:16:40 +0500

    disable create topic button for un-auth users

--- src/components/common/headers/TopicCreationBTN/index.tsx
+++ src/components/common/headers/TopicCreationBTN/index.tsx
@@ -2,23 +2,47 @@ import React from "react";
 import Link from "next/link";
 
 import styles from "./createTopic.module.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "src/store";
 
 const TopicCreationBTN = () => {
+  const { authToken } = useSelector((state: RootState) => ({
+    authToken: state.auth.token,
+  }));
   return (
     <div className={styles.topicBTN} key="topic-btn-area">
-      <Link href="/create/topic" key="create-topic-btn">
-        <a className="ant-btn">
-          {
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              src="/images/topic-icon-orange.svg"
-              alt="svg"
-              className="icon-topic"
-            />
-          }
-          Create Topic
-        </a>
-      </Link>
+      {
+        authToken ? (
+        <>
+        <Link href="/create/topic" key="create-topic-btn">
+          <a className="ant-btn">
+            {
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                src="/images/topic-icon-orange.svg"
+                alt="svg"
+                className="icon-topic"
+              />
+            }
+            Create Topic
+          </a>
+        </Link>
+        </>
+        ): (
+          <>
+            <div className="ant-btn">
+            {
+              <img
+                src="/images/topic-icon-orange.svg"
+                alt="svg"
+                className="icon-topic"
+              />
+            }
+              Create Topic
+            </div>
+          </>
+        )
+      }
     </div>
   );
 };
