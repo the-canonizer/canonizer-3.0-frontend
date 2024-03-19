9a9379e614e3a55af242987ca58a207f42b804b5
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-03-19 10:42:56 +0500

    fix esLint issues in advancesearchFilter, create camp

--- src/pages/camp/create/[...camp].tsx
+++ src/pages/camp/create/[...camp].tsx
@@ -1,14 +1,9 @@
-import { useDispatch } from "react-redux";
 import { getCanonizedAlgorithmsApi } from "src/network/api/homePageApi";
-import { setCanonizedAlgorithms } from "src/store/slices/homePageSlice";
 import CreateNewCamp from "src/components/ComponentPages/CreateNewCamp";
 
 import Layout from "src/hoc/layout";
 
-const CreateNewCampPage = ({ algorithms }: any) => {
-  const dispatch = useDispatch();
-
-  // dispatch(setCanonizedAlgorithms(algorithms));
+const CreateNewCampPage = () => {
   return (
     <Layout routeName={"create-camp"}>
       <CreateNewCamp />
