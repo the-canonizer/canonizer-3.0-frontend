9962569a1129a7b06ea389d994d49fcf96647550
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-04-29 12:16:40 +0500

    test commit

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -2,6 +2,7 @@ import Layout from "src/hoc/layout";
 import My404 from "src/components/ComponentPages/404";
 
 const My404Page = () => {
+  console.log("=====> 404",)
   return (
     <Layout>
       <My404 />
