193fa98920dd9001725947f9c3e41e29e571e4be
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-05-06 18:28:17 +0500

    Topic styling fixed

--- src/components/ComponentPages/TopicDetails/CampTree/index.tsx
+++ src/components/ComponentPages/TopicDetails/CampTree/index.tsx
@@ -249,8 +249,8 @@ const CampTree = ({
           // title="You have subscribed to the entire topic."
           title={
             topicRecord?.topicSubscriptionId &&
-            (data?.title === topicRecord?.topic_name ||
-              data?.review_title === topicRecord?.topic_name)
+              (data?.title === topicRecord?.topic_name ||
+                data?.review_title === topicRecord?.topic_name)
               ? "You have subscribed to the entire topic."
               : `You have subscribed to this camp.`
           }
@@ -262,12 +262,11 @@ const CampTree = ({
         </Tooltip>
       ) : (
         <Tooltip
-          title={`You are subscribed to ${
-            subscribedUsers[userID].child_camp_name
+          title={`You are subscribed to ${subscribedUsers[userID].child_camp_name
               ? subscribedUsers[userID].child_camp_name
               : "child camp."
-          }`}
-          // title="You have subscribed to the entire topic."
+            }`}
+        // title="You have subscribed to the entire topic."
         >
           <i
             className={`icon-subscribe text-secondary  ${styles.implicitIcon}`}
@@ -333,34 +332,29 @@ const CampTree = ({
                         }
                       >
                         <Link
-                          href={`${
-                            includeReview
+                          href={`${includeReview
                               ? isForumPage
                                 ? data[item]?.review_link
-                                    ?.replace("#statement", "")
-                                    ?.replace("/topic/", "/forum/") + "/threads"
-                                : data[item]?.review_link?.replace(
-                                    "#statement",
-                                    ""
-                                  )
-                              : isForumPage
-                              ? data[item]?.link
                                   ?.replace("#statement", "")
                                   ?.replace("/topic/", "/forum/") + "/threads"
-                              : data[item]?.link?.replace("#statement", "")
-                          }?filter=${treeExpandValue}&score=${filterByScore}&algo=${
-                            filterObject?.algorithm
-                          }${
-                            filterObject?.asof == "bydate"
+                                : data[item]?.review_link?.replace(
+                                  "#statement",
+                                  ""
+                                )
+                              : isForumPage
+                                ? data[item]?.link
+                                  ?.replace("#statement", "")
+                                  ?.replace("/topic/", "/forum/") + "/threads"
+                                : data[item]?.link?.replace("#statement", "")
+                            }?filter=${treeExpandValue}&score=${filterByScore}&algo=${filterObject?.algorithm
+                            }${filterObject?.asof == "bydate"
                               ? "&asofdate=" + filterObject?.asofdate
                               : ""
-                          }&asof=${filterObject?.asof}&canon=${
-                            filterObject?.namespace_id
-                          }${viewThisVersion ? "&viewversion=1" : ""}`}
+                            }&asof=${filterObject?.asof}&canon=${filterObject?.namespace_id
+                            }${viewThisVersion ? "&viewversion=1" : ""}`}
                         >
                           <a
-                            className={`${
-                              data[item].is_archive == 1
+                            className={`${data[item].is_archive == 1
                                 ? `font-weight-bold tra ${styles.archive_grey}`
                                 : !isForumPage &&
                                   (data[item]?.camp_id ==
@@ -369,25 +363,24 @@ const CampTree = ({
                                       ?.split("-")
                                       ?.at(0) ??
                                     "1")
-                                ? `font-weight-bold ${styles.activeCamp}`
-                                : ""
-                            } ${
-                              isForumPage &&
-                              data[item]?.camp_id ==
+                                  ? `font-weight-bold ${styles.activeCamp}`
+                                  : ""
+                              } ${isForumPage &&
+                                data[item]?.camp_id ==
                                 ((router?.query?.camp as string)
                                   ?.split("-")
                                   ?.at(0) ?? "1")
                                 ? `font-weight-bold forumActive ${styles.activeCamp}`
                                 : ""
-                            }`}
+                              }`}
                           >
                             {data[item].is_archive == 1 ? (
                               <Popover content="Archived Camp">
                                 {includeReview
                                   ? data[item]?.review_title
                                   : data[item].camp_id === 1
-                                  ? "Agreement"
-                                  : data[item]?.title}
+                                    ? "Agreement"
+                                    : data[item]?.title}
                               </Popover>
                             ) : includeReview ? (
                               data[item]?.review_title
@@ -425,15 +418,14 @@ const CampTree = ({
                           width={String(
                             showScoreBars
                               ? (data[item].score * 460) /
-                                  tree?.at(0)["1"].score +
-                                  50 +
-                                  "px"
-                              : `${
-                                  (is_checked
-                                    ? data[item].full_score?.toFixed(2)
-                                    : data[item].score?.toFixed(2)
-                                  ).length * 11
-                                }px`
+                              tree?.at(0)["1"].score +
+                              50 +
+                              "px"
+                              : `${(is_checked
+                                ? data[item].full_score?.toFixed(2)
+                                : data[item].score?.toFixed(2)
+                              ).length * 11
+                              }px`
                           )}
                           baseBgColor={"#fff"}
                           labelAlignment={"left"}
@@ -474,14 +466,13 @@ const CampTree = ({
                               pathname: `/camp/create/${replaceSpecialCharacters(
                                 router?.query.camp[0],
                                 "-"
-                              )}/${
-                                router?.query.camp[1]
+                              )}/${router?.query.camp[1]
                                   ? replaceSpecialCharacters(
-                                      router?.query.camp[1],
-                                      "-"
-                                    )
+                                    router?.query.camp[1],
+                                    "-"
+                                  )
                                   : 1
-                              }`,
+                                }`,
                             }}
                           >
                             <a>{`<Start new supporting camp here>`} </a>
@@ -540,81 +531,78 @@ const CampTree = ({
 
   return tree?.at(0) ? (
     (showTree && tree?.at(0)["1"]?.title != "" && defaultExpandKeys) ||
-    isForumPage ? (
+      isForumPage ? (
       <>
         <Typography.Paragraph
           className={`${styles.topicTitleStyle} ${styles.topicTitle}`}
         >
           <div className="event-line-wrapper">
-          <span className="normal">Topic : </span>
-          <Button
-                            type="primary"
-                            size="small"
-                            onClick={eventLinePath}
-                            className={styles.btnCampForum}
-                            id="camp-forum-btn"
-                          >
-                            Event Line
-                          </Button>
-          </div>
-          {tree?.length && tree[0] ? (
-            <Link
-              href={`${
-                includeReview
-                  ? isForumPage
-                    ? tree[0]["1"]?.review_link
-                        ?.replace("#statement", "")
-                        ?.replace("/topic/", "/forum/") + "/threads"
-                    : tree[0]["1"]?.review_link?.replace("#statement", "")
-                  : isForumPage
-                  ? tree[0]["1"]?.link
-                      ?.replace("#statement", "")
-                      ?.replace("/topic/", "/forum/") + "/threads"
-                  : tree[0]["1"]?.link?.replace("#statement", "")
-              }?filter=${treeExpandValue}&score=${filterByScore}&algo=${
-                filterObject?.algorithm
-              }${
-                filterObject?.asof == "bydate"
-                  ? "&asofdate=" + filterObject?.asofdate
-                  : ""
-              }&asof=${filterObject?.asof}&canon=${filterObject?.namespace_id}${
-                viewThisVersion ? "&viewversion=1" : ""
-              }`}
-              className={styles.boldBreadcrumb}
-              replace
+            <div>
+              <span className="normal">Topic : </span>
+              {tree?.length && tree[0] ? (
+                <Link
+                  href={`${includeReview
+                      ? isForumPage
+                        ? tree[0]["1"]?.review_link
+                          ?.replace("#statement", "")
+                          ?.replace("/topic/", "/forum/") + "/threads"
+                        : tree[0]["1"]?.review_link?.replace("#statement", "")
+                      : isForumPage
+                        ? tree[0]["1"]?.link
+                          ?.replace("#statement", "")
+                          ?.replace("/topic/", "/forum/") + "/threads"
+                        : tree[0]["1"]?.link?.replace("#statement", "")
+                    }?filter=${treeExpandValue}&score=${filterByScore}&algo=${filterObject?.algorithm
+                    }${filterObject?.asof == "bydate"
+                      ? "&asofdate=" + filterObject?.asofdate
+                      : ""
+                    }&asof=${filterObject?.asof}&canon=${filterObject?.namespace_id}${viewThisVersion ? "&viewversion=1" : ""
+                    }`}
+                  className={styles.boldBreadcrumb}
+                  replace
+                >
+                  <a
+                    className={`${tree[0]["1"].is_archive == 1
+                        ? `font-weight-bold tra ${styles.archive_grey}`
+                        : tree[0]["1"]?.camp_id ==
+                          router?.query?.camp?.at(1)?.split("-")?.at(0) ?? "1"
+                          ? `font-weight-bold ${styles.activeCamp}`
+                          : ""
+                      } ${isForumPage &&
+                        tree[0]["1"]?.camp_id ==
+                        ((router?.query?.camp as string)?.split("-")?.at(0) ?? "1")
+                        ? `font-weight-bold forumActive ${styles.activeCamp}`
+                        : ""
+                      }`}
+                  >
+                    {tree[0]["1"].is_archive == 1 ? (
+                      <Popover content="Archived Camp">
+                        {includeReview
+                          ? tree[0]["1"]?.review_title
+                          : tree[0]["1"]?.title}
+                      </Popover>
+                    ) : includeReview ? (
+                      tree[0]["1"]?.review_title
+                    ) : (
+                      tree[0]["1"]?.title
+                    )}
+                  </a>
+                </Link>
+              ) : (
+                ""
+              )}{" "}
+
+            </div>
+            <Button
+              type="primary"
+              size="small"
+              onClick={eventLinePath}
+              className={styles.btnCampForum}
+              id="camp-forum-btn"
             >
-              <a
-                className={`${
-                  tree[0]["1"].is_archive == 1
-                    ? `font-weight-bold tra ${styles.archive_grey}`
-                    : tree[0]["1"]?.camp_id ==
-                        router?.query?.camp?.at(1)?.split("-")?.at(0) ?? "1"
-                    ? `font-weight-bold ${styles.activeCamp}`
-                    : ""
-                } ${
-                  isForumPage &&
-                  tree[0]["1"]?.camp_id ==
-                    ((router?.query?.camp as string)?.split("-")?.at(0) ?? "1")
-                    ? `font-weight-bold forumActive ${styles.activeCamp}`
-                    : ""
-                }`}
-              >
-                {tree[0]["1"].is_archive == 1 ? (
-                  <Popover content="Archived Camp">
-                    {includeReview
-                      ? tree[0]["1"]?.review_title
-                      : tree[0]["1"]?.title}
-                  </Popover>
-                ) : includeReview ? (
-                  tree[0]["1"]?.review_title
-                ) : (
-                  tree[0]["1"]?.title
-                )}
-              </a>
-            </Link>
-          ) : (
-            ""
-          )}{" "}
+              Event Line
+            </Button>
+          </div>
           <span className={styles.subScriptionIcon}>
             {isUserAuthenticated && !!topicRecord?.topicSubscriptionId ? (
               <Tooltip
