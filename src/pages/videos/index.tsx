fd8d0d067a496e1add0aaa37b95b63a366dc773e
wahaj.hafeez <wahaj.hafeez@codedistrict.com>
2024-05-14 19:45:30 +0500

    skeleton added for videos

--- src/pages/videos/index.tsx
+++ src/pages/videos/index.tsx
@@ -11,82 +11,15 @@ import { CaretRightOutlined } from "@ant-design/icons";
 import { getVideosContentApi } from "src/network/api/videos";
 import { replaceSpecialCharacters } from "src/utils/generalUtility";
 import Skeleton from "react-loading-skeleton";
+import CustomSkeleton from "@/components/common/customSkelton";
+
 // eslint-disable-next-line @next/next/no-img-element
 const { Meta } = Card;
 const VideosPage = () => {
   const router = useRouter();
   const [categories, setCategories] = useState(null);
   const [loader, setLoader] = useState(false);
-  const [loaderData, setLoaderData] = useState([
-    {
-      id: 1,
-      title: "Consciousness: Not a Hard Problem, Just a Color Problem",
-      type: "video",
-      videos: [
-        {
-          id: 1,
-          title: "Introduction",
-          thumbnail: "introduction_thumb.png",
-        },
-        {
-          id: 2,
-          title: "Perceiving a Strawberry",
-          thumbnail: "perceiving_a_strawberry_thumb.png",
-        },
-        {
-          id: 3,
-          title: "Differentiating Reality and Knowledge of Reality",
-          thumbnail: "differentiate_reality_knowledge_thumb.png",
-        },
-        {
-          id: 4,
-          title: "The world in your head",
-          thumbnail: "the_world_in_your_head_thumb.png",
-        },
-        {
-          id: 5,
-          title: "The Perception Of Size",
-          thumbnail: "the_perception_of_size_thumb.png",
-        },
-        {
-          id: 6,
-          title: "Computational Binding",
-          thumbnail: "computational_binding_thumb.png",
-        },
-        {
-          id: 7,
-          title: "Cognitive Knowledge",
-          thumbnail: "cognitive_knowledge_thumb.png",
-        },
-        {
-          id: 8,
-          title: "Simulation Hypothesis",
-          thumbnail: "simulation_hypothesis_thumb.png",
-        },
-        {
-          id: 9,
-          title: "Representational Qualia Theory Consensus",
-          thumbnail: "representational_qualia_consensus_thumb.png",
-        },
-        {
-          id: 10,
-          title: "Conclusion",
-          thumbnail: "conclusion_thumb.png",
-        },
-        {
-          id: 10,
-          title: "Conclusion",
-          thumbnail: "conclusion_thumb.png",
-        },
-        {
-          id: 10,
-          title: "Conclusion",
-          thumbnail: "conclusion_thumb.png",
-        },
-      ],
-    },
-  ]);
-
+  
   const onCardClick = (e, video) => {
     e.stopPropagation();
     router.push(
@@ -108,40 +41,11 @@ const VideosPage = () => {
   return (
     <Layout routeName={"video"}>
       <div className="pageContentWrap">
+
         {loader ? (
-          <>
-            {loaderData?.map((category) => {
-              return (
-                <>
-                  <Card
-                    className="video-parent-card"
-                    title={<Skeleton width={460} count={1} enableAnimation />}
-                  >
-                    <Row gutter={[16, 16]}>
-                      {category?.videos?.map((video) => {
-                        return (
-                          <>
-                            <Col xs={24} sm={12} lg={8} xl={6}>
-                              <Card
-                                className="video-inner-card"
-                                cover={
-                                  <>
-                                    <Skeleton style={{ height: "200px" }} />
-                                  </>
-                                }
-                              >
-                                <Meta title={<Skeleton />} />
-                              </Card>
-                            </Col>
-                          </>
-                        );
-                      })}
-                    </Row>
-                  </Card>
-                </>
-              );
-            })}
-          </>
+            <CustomSkeleton
+              skeltonFor="videos"
+            />
         ) : (
           <>
             {categories?.map((category) => {
@@ -163,7 +67,7 @@ const VideosPage = () => {
                                     <img
                                       alt=""
                                       src={
-                                        process.env.NEXT_PUBLIC_BASE_VIDEOS_URL +"videos/consciousness/" +
+                                        process.env.NEXT_PUBLIC_BASE_VIDEOS_URL + "videos/consciousness/" +
                                         video.thumbnail
                                       }
                                     />
