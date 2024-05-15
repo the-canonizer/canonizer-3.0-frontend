482bd800f6372a511aa27dd3a2f173f649694ca7
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2024-05-15 17:39:45 +0500

    ui of share button on event line page

--- src/components/ComponentPages/TimeLine/index.tsx
+++ src/components/ComponentPages/TimeLine/index.tsx
@@ -1,6 +1,8 @@
+import styles from "./timeline.module.scss";
 import React, { useEffect, useState } from "react";
 import RacingBarChart from "./RacingBarChart";
 import useInterval from "./useInterval";
+import type { RadioChangeEvent } from 'antd';
 // import "./App.css";
 import TimelineSlider from "../eventLine/TimelineSlider";
 import { getEventLineApi } from "src/network/api/topicEventLineAPI";
@@ -8,10 +10,20 @@ import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { RootState } from "src/store/index.js";
 import CustomSkelton from "../../common/customSkelton";
-import { Empty } from "antd";
+import { Button, Empty, Popover,  Radio, Space, Typography, Tooltip  } from "antd";
+import { CopyOutlined, FacebookOutlined, LinkedinOutlined, ShareAltOutlined, TwitterOutlined } from "@ant-design/icons";
+import FacebookIcon from '../../../assets/image/social-media-icons/facebook.png';
+import LinkedinIcon from '../../../assets/image/social-media-icons/linkedin.png';
+import TwitterIcon from '../../../assets/image/social-media-icons/twitter.png';
+import CheckIcon from '../../../assets/image/social-media-icons/check.png';
+import CopyLinkIcon from '../../../assets/image/social-media-icons/copy-link.png';
+
 // const getRandomIndex = (array) => {
 //   return Math.floor(array.length * Math.random());
 // };
+
+
+const { Title } = Typography;
 
 function TimeLine({ setTimelineDescript, setLoadingEvents }: any) {
   const [loading, setLoading] = useState(false);
@@ -22,6 +34,12 @@ function TimeLine({ setTimelineDescript, setLoadingEvents }: any) {
   const [animationSpeed, setAnimationSpeed] = useState(1000);
   const [isPlaying, setIsPlaying] = useState(false);
   const router = useRouter();
+  const [value, setValue] = useState(1);
+
+  const onChange = (e: RadioChangeEvent) => {
+    console.log('radio checked', e.target.value);
+    setValue(e.target.value);
+  };
 
   const events = mockData && Object.keys(mockData).sort();
   const [data, setData] = useState([]);
@@ -115,6 +133,44 @@ function TimeLine({ setTimelineDescript, setLoadingEvents }: any) {
     );
     // setEventDescription(mockData[events[iteration]].event?.message);
   };
+
+  const content = (
+    <div className={styles.popoverWrapper}>
+    <Radio.Group onChange={onChange} value={value}>
+    <Space direction="vertical">
+      <Radio value={1}>Copy URL</Radio>
+      <Radio value={2}>Copy URL at Current Time</Radio>
+      
+    </Space>
+  </Radio.Group>
+  <Space align={"center"} className={styles.shareLink} >
+    <Title level={5}> Share Link to: </Title>
+    <Space >
+      {/* <Tooltip title="Copy Link">
+        <Button type="primary" shape="circle" size="small" icon={<CopyOutlined />} />
+      </Tooltip> */}
+      <Tooltip title="Share On Facebook">
+      <img src={FacebookIcon.src} className={styles.cursorPointer}/>
+      </Tooltip>
+      <Tooltip title="Share On Twitter">
+      <img src={TwitterIcon.src} className={styles.cursorPointer}/>
+      </Tooltip>
+      <Tooltip title="Share On Linkedin">
+      <img src={LinkedinIcon.src} className={styles.cursorPointer}/>
+      </Tooltip>
+      <Tooltip title="Copy Link">
+      <img src={CopyLinkIcon.src} className={styles.cursorPointer}/>
+      </Tooltip>
+      {/* <Tooltip title="Share On Linkedin">
+      <img src={CheckIcon.src} className={styles.cursorPointer}/>
+      </Tooltip> */}
+
+
+      
+      </Space>
+  </Space>
+  </div>
+  );
   return (
     <React.Fragment>
       {loading || !data ? (
@@ -125,6 +181,11 @@ function TimeLine({ setTimelineDescript, setLoadingEvents }: any) {
           stylingClass=""
         />
       ) : (
+        <>
+        <Popover content={content} trigger="click" placement="bottomRight" showArrow={false} >
+            <Button type="primary" size="middle" className={styles.btnShareURL} ghost icon={<ShareAltOutlined />}>Share</Button>
+        </Popover>
+         
         <TimelineSlider
           mockData={mockData}
           setStart={setStart}
@@ -139,6 +200,7 @@ function TimeLine({ setTimelineDescript, setLoadingEvents }: any) {
           isPlaying={isPlaying}
           setIsPlaying={setIsPlaying}
         />
+        </>
       )}
       <div className="evenline-bars">
         {loading || !data ? (
