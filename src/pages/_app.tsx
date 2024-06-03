3468798b0cea9bd6bd74c54caa44b9cd96473e7e
wahaj.hafeez <wahaj.hafeez@codedistrict.com>
2024-06-03 12:27:06 +0500

    persist loginToken

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -50,13 +50,19 @@ function WrappedApp({
  
  
    const refreshCacheAndReload = () => {
-      localStorage.clear();
+      for (const key in localStorage) {
+        if (key !== "auth_token") {
+          localStorage.removeItem(key);
+        }
+      }
      
       const cookies = document.cookie.split("; ");
         for (let cookie of cookies) {
           const eqPos = cookie.indexOf("=");
           const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-          document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+          if (name !== "loginToken"){
+            document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+          }
         }
  
  
