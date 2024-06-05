285b42f808e75853a8efe41f52cc7b567ff3fb5d
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-06-05 12:32:12 +0500

    revert code

--- src/components/ComponentPages/TopicDetails/index.tsx
+++ src/components/ComponentPages/TopicDetails/index.tsx
@@ -148,6 +148,19 @@ const TopicDetails = ({ serverSideCall }: any) => {
   }
 
 
+  useEffect(()=>{
+
+  const parts = router?.query?.camp[0]?.split("-");
+  const result = parts?.slice(1)?.join("-");
+  const topic_Name = result?.split("-")?.join(" ");
+
+  if(tree && (tree[1]?.topic_id == parts[0] && tree[1]?.title != topic_Name)){
+    // let asPath=tree[1]?.link
+    const asPath = `${tree[1].topic_id}-${replaceSpecialCharacters(tree[1].title, "-")}/1-Agreement`;    
+     router.push(router.pathname,asPath)
+ }    
+  },[router?.query?.camp,tree])
+
   useEffect(() => {
     async function getTreeApiCall() {
       if (!showTreeSkeltonRef) {
@@ -456,20 +469,6 @@ const TopicDetails = ({ serverSideCall }: any) => {
   useEffect(() => {
     setIsClient(true);
   }, []);
-
-
-  useEffect(()=>{
-
-    const parts = router?.query?.camp[0]?.split("-");
-    const result = parts?.slice(1)?.join("-");
-    const topic_Name = result?.split("-")?.join(" ");
-  
-    if(tree && (tree[1]?.topic_id == parts[0] && tree[1]?.title != topic_Name)){
-      // let asPath=tree[1]?.link
-      const asPath = `${tree[1].topic_id}-${replaceSpecialCharacters(tree[1].title, "-")}/1-Agreement`;    
-       router.push(router.pathname,asPath)
-   }    
-    },[router?.query?.camp,tree])
 
   const lable = algorithms?.find((obj) => {
     return obj.algorithm_key == selectedAlgorithm;
