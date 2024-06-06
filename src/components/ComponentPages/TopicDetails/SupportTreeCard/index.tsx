85e0e27be620674158d09185658be6b457d53214
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-06-06 17:58:52 +0500

    fix issue 1802

--- src/components/ComponentPages/TopicDetails/SupportTreeCard/index.tsx
+++ src/components/ComponentPages/TopicDetails/SupportTreeCard/index.tsx
@@ -423,14 +423,14 @@ const SupportTreeCard = ({
                               content={
                                 !isUserAuthenticated
                                   ? "Log in to participate"
-                                  : ""
+                                  : "This will delegate your support to the selected supporter"
                               }
                             >
                               <a className="printHIde">
-                                <Tooltip
+                                {/* <Tooltip
                                   title="This will delegate your support to the selected supporter"
                                   placement="right"
-                                >
+                                > */}
                                   <Button
                                     id="supportTreeDelegateYourSupport"
                                     disabled={
@@ -445,7 +445,6 @@ const SupportTreeCard = ({
                                   >
                                     {"Delegate Your Support"}
                                   </Button>
-                                </Tooltip>
 
                               </a>
                             </Popover>
