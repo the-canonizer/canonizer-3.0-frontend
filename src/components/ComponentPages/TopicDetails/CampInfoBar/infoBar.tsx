2de340c10ad6149d8f216d8f36b859f836ec10fb
vikasrana <vikas.rana@talentelgia.in>
2024-06-12 17:07:15 +0530

    Ticket #929
    Three Dots" option is not working for all topics in certain scenario.

--- src/components/ComponentPages/TopicDetails/CampInfoBar/infoBar.tsx
+++ src/components/ComponentPages/TopicDetails/CampInfoBar/infoBar.tsx
@@ -564,7 +564,7 @@ const InfoBar = ({
                       >
                         <a
                           className={styles.iconMore}
-                          onClick={(e) => e.preventDefault()}
+                          onClick={(e) => {e.preventDefault(); dispatch(setManageSupportStatusCheck(false))}}
                         >
                           <MoreOutlined />
                         </a>
