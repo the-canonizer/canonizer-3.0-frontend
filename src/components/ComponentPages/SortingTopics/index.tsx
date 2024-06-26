1d18d7a6ba5aa76edb87b84623e4b6333a0a9d94
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-06-26 17:55:42 +0500

    ui in proggress

--- src/components/ComponentPages/SortingTopics/index.tsx
+++ src/components/ComponentPages/SortingTopics/index.tsx
@@ -1,6 +1,6 @@
-import { Dropdown } from "antd";
-import type { MenuProps } from "antd";
+import { Dropdown, Select } from "antd";
 import Image from "next/image";
+import { SortDescendingOutlined } from "@ant-design/icons";
 import SortActiveTopicIcon from "../../../assets/image/sort-active-ico.svg";
 import SortTopicIcon from "../../../assets/image/sort-ico.svg";
 import styles from "../../ComponentPages/SortingTopics/sort.module.scss";
@@ -14,74 +14,122 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const SortTopics = () => {
-  const { sortLatestTopic, sortScoreViewTopic } = useSelector(
-    (state: RootState) => ({
-      sortLatestTopic: state?.utils?.sortLatestTopic,
-      sortScoreViewTopic: state?.utils?.sortScoreViewTopic,
-    })
-  );
+  const dispatch = useDispatch();
   const router = useRouter();
 
+  // State from Redux
+  const { sortLatestTopic, sortScoreViewTopic } = useSelector(
+    (state: RootState) => ({
+      sortLatestTopic: state.utils.sortLatestTopic,
+      sortScoreViewTopic: state.utils.sortScoreViewTopic,
+    })
+  );
+
+  // Reset sorting options when navigating to the home page
   useEffect(() => {
-    if (router.pathname == "/") {
+    if (router.pathname === "/") {
       dispatch(setScoreViewTopic(false));
       dispatch(setSortLatestTopic(false));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  const dispatch = useDispatch();
-  const onLatestTopic = () => {
+  }, [router.pathname]);
+
+  // Handlers for selecting sorting options
+  const handleLatestTopic = () => {
     dispatch(setSortLatestTopic(true));
     dispatch(setScoreViewTopic(false));
   };
-  const onScoreViewTopic = () => {
+
+  const handleScoreViewTopic = () => {
     dispatch(setScoreViewTopic(true));
     dispatch(setSortLatestTopic(false));
   };
-  const items: MenuProps["items"] = [
-    {
-      key: "1",
-      label: (
-        <a onClick={onLatestTopic}>
-          <div>Latest</div>
-        </a>
-      ),
-    },
-    {
-      type: "divider",
-    },
-    {
-      key: "2",
-      label: (
-        <a onClick={onScoreViewTopic}>
-          <div>Score Value</div>
-        </a>
-      ),
-    },
-  ];
+
+  // Dropdown menu items
+  // const dropdownMenuItems = [
+  //   {
+  //     key: "latest",
+  //     label: "Latest",
+  //     onClick: handleLatestTopic,
+  //   },
+  //   {
+  //     type: "divider",
+  //   },
+  //   {
+  //     key: "scoreValue",
+  //     label: "Score Value",
+  //     onClick: handleScoreViewTopic,
+  //   },
+  // ];
 
   return (
-    <Dropdown menu={{ items }} trigger={["click"]}>
-      <a onClick={(e) => e.preventDefault()} className={styles.sort_topic}>
-        <span>
-          {sortLatestTopic
+    <div className={styles.sortContainer}>
+      {/* Select component for sorting */}
+      <Select
+        size="large"
+        suffixIcon={<SortDescendingOutlined />}
+        style={{ width: 150 }}
+        placeholder="Sort By"
+        value={
+          sortLatestTopic
             ? "Latest"
             : sortScoreViewTopic
-            ? "Score Value"
-            : "Sort"}
-          <Image
-            width={20}
-            height={20}
-            alt="sort"
-            src={
-              sortLatestTopic || sortScoreViewTopic
-                ? SortActiveTopicIcon
-                : SortTopicIcon
-            }
-          />
-        </span>
-      </a>
-    </Dropdown>
+            ? "ScoreValue"
+            : undefined
+        }
+        onChange={(value) => {
+          if (value === "Latest") {
+            handleLatestTopic();
+          } else if (value === "ScoreValue") {
+            handleScoreViewTopic();
+          }
+        }}
+      >
+        <Select.Option value="Latest">Latest</Select.Option>
+        <Select.Option value="ScoreValue">Score Value</Select.Option>
+      </Select>
+      {/* Dropdown for mobile or additional options */}
+      {/* <Dropdown
+        overlay={
+          <div className={styles.dropdownMenu}>
+            {dropdownMenuItems.map((item) =>
+              item.type === "divider" ? (
+                <div key={item.key} className={styles.dropdownDivider} />
+              ) : (
+                <a key={item.key} onClick={item.onClick}>
+                  {item.label}
+                </a>
+              )
+            )}
+          </div>
+        }
+        trigger={["click"]}
+      >
+        <a
+          href="#"
+          onClick={(e) => e.preventDefault()}
+          className={styles.sortTopic}
+        >
+          <span>
+            {sortLatestTopic
+              ? "Latest"
+              : sortScoreViewTopic
+              ? "Score Value"
+              : "Sort"}
+            <Image
+              width={20}
+              height={20}
+              alt="sort"
+              src={
+                sortLatestTopic || sortScoreViewTopic
+                  ? SortActiveTopicIcon
+                  : SortTopicIcon
+              }
+            />
+          </span>
+        </a>
+      </Dropdown> */}
+    </div>
   );
 };
 
