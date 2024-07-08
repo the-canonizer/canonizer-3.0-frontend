c9b1dd930208d5e471f13da4517bf7d60b4ee8f2
Rahul Kumar Singh <rahul.singh@iffort.com>
2024-07-08 16:59:05 +0530

    ui fixes.

--- src/components/common/headers/HeaderMenu/index.tsx
+++ src/components/common/headers/HeaderMenu/index.tsx
@@ -9,7 +9,6 @@ import {
   SettingOutlined,
   HomeOutlined,
   QuestionCircleOutlined,
-  PlusOutlined,
   GlobalOutlined,
   CloseOutlined,
   ArrowRightOutlined,
@@ -29,7 +28,6 @@ import { logout } from "src/network/api/userApi";
 import { getGravatarPicApi } from "src/network/api/notificationAPI";
 import Logo from "../logoHeader";
 import { useIsMobile } from "src/hooks/useIsMobile";
-import SecondaryButton from "components/shared/Buttons/SecondaryButton";
 
 const menuItems = [
   {
@@ -75,6 +73,8 @@ const menuItems = [
     linkTitle: "Notifications",
     id: 4,
     icon: <BellOutlined />,
+    isAuthReq: true,
+    isMobile: true,
   },
   {
     link: "/settings",
@@ -82,6 +82,7 @@ const menuItems = [
     id: 5,
     isMobile: true,
     icon: <SettingOutlined />,
+    isAuthReq: true,
   },
   {
     link: "/settings?tab=supported_camps",
@@ -89,6 +90,7 @@ const menuItems = [
     id: 5,
     isMobile: true,
     icon: <CheckCircleOutlined />,
+    isAuthReq: true,
   },
 ];
 
@@ -229,7 +231,11 @@ const HeaderMenu = ({ className = "", isUserAuthenticated }) => {
                   ? `border-[1px] px-3 py-2 rounded-lg border-canBlue bg-[#98B7E61A] ${
                       isMobile ? "bg-canBlue text-white rounded-md" : ""
                     }`
-                  : `hover:text-canHoverBlue`
+                  : `hover:text-canHoverBlue] ${
+                      isMobile
+                        ? "bg-canBlue text-white rounded-md px-3 py-2"
+                        : ""
+                    }`
               }
               isWithIcon={isUserAuthenticated}
             />
@@ -248,7 +254,7 @@ const HeaderMenu = ({ className = "", isUserAuthenticated }) => {
                   item.linkTitle?.toLowerCase() === "browse"
                     ? "before:!hidden after:!hidden"
                     : ""
-                }`}
+                } ${item?.isAuthReq && !isUserAuthenticated ? "hidden" : ""}`}
                 key={item.id + "_" + item.link + "___" + idx}
               >
                 <Link href={item.link}>
@@ -291,7 +297,7 @@ const HeaderMenu = ({ className = "", isUserAuthenticated }) => {
             </ListItem>
           )}
         </ul>
-        {isMobile ? (
+        {isMobile && isUserAuthenticated ? (
           <div className="mt-auto flex justify-between items-center">
             <div className="flex items-center">
               <ProfileInfoTab
@@ -348,7 +354,7 @@ const HeaderMenu = ({ className = "", isUserAuthenticated }) => {
       ) : null} */}
       <Button
         size="middle"
-        className="border-0 p-0 block -mt-2 lg:hidden ml-2 md:ml-0"
+        className="border-0 p-0 block -mt-2 lg:hidden ml-2 md:ml-auto"
         onClick={toggleMobNav}
         key="outnline-btn"
       >
