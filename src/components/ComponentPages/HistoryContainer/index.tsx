8f1a12584dd1acf2e9dcea6f1e41eb0e36491c49
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-07-12 17:13:05 +0500

    Bread crumb issue

--- src/components/ComponentPages/HistoryContainer/index.tsx
+++ src/components/ComponentPages/HistoryContainer/index.tsx
@@ -241,17 +241,16 @@ function HistoryContainer() {
 
   const onCompareClick = () => {
     router?.push({
-      pathname: `/statement/compare/${router?.query.camp[0]}/${
-        router?.query.camp[1] ? router?.query.camp[1] : "1-Agreement"
-      }`,
+      pathname: `/statement/compare/${router?.query.camp[0]}/${router?.query.camp[1] ? router?.query.camp[1] : "1-Agreement"
+        }`,
       query: {
         statements: selectedTopic?.at(0) + "_" + selectedTopic?.at(1),
         from:
           historyOf == "statement"
             ? "statement"
             : historyOf == "camp"
-            ? "camp"
-            : "topic",
+              ? "camp"
+              : "topic",
         status: selectedTopicStatus.join("-"),
       },
     });
@@ -385,9 +384,8 @@ function HistoryContainer() {
             <div className="sider-btn">
               <Button
                 size="large"
-                className={`btn-all min-w-[133px] ${
-                  activeTab == "all" ? " active" : null
-                }`}
+                className={`btn-all min-w-[133px] ${activeTab == "all" ? " active" : null
+                  }`}
                 onClick={() => {
                   handleTabButton("all");
                 }}
@@ -399,9 +397,8 @@ function HistoryContainer() {
               </Button>
               <Button
                 size="large"
-                className={`btn-objected min-w-[133px] ${
-                  activeTab == "objected" ? " active" : null
-                }`}
+                className={`btn-objected min-w-[133px] ${activeTab == "objected" ? " active" : null
+                  }`}
                 onClick={() => {
                   handleTabButton("objected");
                 }}
@@ -413,9 +410,8 @@ function HistoryContainer() {
               </Button>
               <Button
                 size="large"
-                className={`btn-live min-w-[133px] ${
-                  activeTab == "live" ? " active" : null
-                }`}
+                className={`btn-live min-w-[133px] ${activeTab == "live" ? " active" : null
+                  }`}
                 onClick={() => {
                   handleTabButton("live");
                 }}
@@ -425,9 +421,8 @@ function HistoryContainer() {
               </Button>
               <Button
                 size="large"
-                className={`btn-pending min-w-[133px] ${
-                  activeTab == "in_review" ? " active" : null
-                }`}
+                className={`btn-pending min-w-[133px] ${activeTab == "in_review" ? " active" : null
+                  }`}
                 onClick={() => {
                   handleTabButton("in_review");
                 }}
@@ -439,9 +434,8 @@ function HistoryContainer() {
               </Button>
               <Button
                 size="large"
-                className={`btn-previous min-w-[133px] ${
-                  activeTab == "old" ? " active" : null
-                }`}
+                className={`btn-previous min-w-[133px] ${activeTab == "old" ? " active" : null
+                  }`}
                 onClick={() => {
                   handleTabButton("old");
                 }}
