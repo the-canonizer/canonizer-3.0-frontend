d1c5062277480b6eb60ebb3bb124e41ee3721b6d
adeel-ahmed-cd <adeel.ahmed@codedistrict.com>
2024-07-22 17:57:46 +0500

    notification complete

---
+++ src/components/ComponentPages/notificationBar/notificationBar.tsx
@@ -0,0 +1,21 @@
+import { CheckCircleFilled } from "@ant-design/icons";
+import { Button, notification } from "antd";
+import React from "react";
+const openNotificationWithIcon = (type) => {
+  notification[type]({
+    duration: 5,
+    closeIcon: null,
+    className: "thm-notification",
+    icon: <CheckCircleFilled />,
+
+    description:
+      "Your support from Camp: New research & Debating the theory has been removed.",
+  });
+};
+function NotificationBar() {
+  return (
+    <Button onClick={() => openNotificationWithIcon("success")}>Success</Button>
+  );
+}
+
+export default NotificationBar;
