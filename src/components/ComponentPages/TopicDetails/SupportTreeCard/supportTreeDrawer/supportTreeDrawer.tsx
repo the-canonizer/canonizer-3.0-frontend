69893b08bbb94711be642a89dc11d4cc97d1fb31
wahaj.hafeez <wahaj.hafeez@codedistrict.com>
2024-08-02 16:13:25 +0500

    fixed remove_camps issue

--- src/components/ComponentPages/TopicDetails/SupportTreeCard/supportTreeDrawer/supportTreeDrawer.tsx
+++ src/components/ComponentPages/TopicDetails/SupportTreeCard/supportTreeDrawer/supportTreeDrawer.tsx
@@ -272,7 +272,7 @@ function SupportTreeDrawer({
     if (shouldRemoveSupport() && supportedCampsStatus?.support_flag == 1) {
       let payload = {
         topic_num: reqBodyData.topic_num,
-        remove_camps: supportedCampsStatus?.support_flag == 1 && removeSupportFromCamps(),
+        remove_camps: supportedCampsStatus?.support_flag == 1? removeSupportFromCamps():[],
         type: "direct",
         action: "all",
         nick_name_id: nictNameId,
@@ -296,7 +296,7 @@ function SupportTreeDrawer({
       let payload = {
         topic_num: topicNum,
         add_camp: { camp_num: camp_num, support_order: tagsArrayList?.length },
-        remove_camps: supportedCampsStatus?.support_flag == 1 && removeSupportFromCamps(),
+        remove_camps: supportedCampsStatus?.support_flag == 1 ? removeSupportFromCamps():[],
         type: "direct",
         action: removeSupportFromCamps()?.length > 0 ? "partial" : "add",
         nick_name_id: nictNameId,
