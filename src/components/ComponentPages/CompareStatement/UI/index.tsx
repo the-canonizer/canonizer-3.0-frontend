202a1e1263dfa6dcc425cc11b871a663cf517a93
wahaj.hafeez <wahaj.hafeez@codedistrict.com>
2024-08-19 14:12:44 +0500

    update the ui and make tabs sticky

--- src/components/ComponentPages/CompareStatement/UI/index.tsx
+++ src/components/ComponentPages/CompareStatement/UI/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Button, Row, Col } from "antd";
+import { Button, Row, Col, Tabs } from "antd";
 import styles from "./index.module.scss";
 
 import CustomSkelton from "../../../common/customSkelton";
@@ -8,6 +8,7 @@ import { capitalizeFirstLetter } from "src/utils/generalUtility";
 import Breadcrumbs from "components/ComponentPages/Breadcrumbs/breadcrumbs";
 import HistoryCard from "components/ComponentPages/HistoryCard/historyCard";
 import ComparisonMobileTabs from "./comparisonMobileTabs";
+import moment from "moment";
 const validUrl = (url) => {
   try {
     new URL(url);
@@ -29,6 +30,36 @@ function CompareStatementUI({
   const s1 = statements?.at(0) || {},
     s2 = statements?.at(1) || {},
     from = router?.query?.from;
+
+  const breakpoint = 768;
+
+  // Initialize state without using window.innerWidth directly
+  const [isMobileView, setIsMobileView] = useState(
+    typeof window !== "undefined" ? window.innerWidth < breakpoint : false
+  );
+
+  console.log("isMobileView", isMobileView);
+
+  const convertToTime = (unixTime) => {
+    return moment(unixTime * 1000).format("hh:mm:ss A");
+  };
+  
+  const convertToDate = (unixTime) => {
+    return moment(unixTime * 1000).format("DD MMMM YYYY");
+  };
+  
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < breakpoint);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [breakpoint]);
 
   let payload = {
     camp_num: router?.query?.routes[1]?.split("-")[0] ?? "1",
@@ -54,10 +85,8 @@ function CompareStatementUI({
 
   return (
     <>
-      <Breadcrumbs
-        compareMode={compareMode}
-        historyOF={router && router?.query?.from}
-      />
+      <Breadcrumbs compareMode={compareMode} historyOF={router?.query?.from} />
+
       {isLoading ? (
         <CustomSkelton skeltonFor="comparisonPage" />
       ) : (
@@ -71,34 +100,83 @@ function CompareStatementUI({
             {router?.query?.from && capitalizeFirstLetter(router?.query?.from)}{" "}
             History Comparison
           </Button>
-          <Row gutter={[24, 24]}>
-            <Col xs={24} md={12}>
-              <HistoryCard
-                compareMode={compareMode}
-                comparisonData={s1}
-                status={itemsStatus[s1?.id]}
-                s1={true}
-              />
-            </Col>
-            <Col xs={24} md={12}>
-              <HistoryCard
-                compareMode={compareMode}
-                comparisonData={s2}
-                status={itemsStatus[s2?.id]}
-              />
-            </Col>
-            <Col xs={24} md={24}>
-              {liveStatement !== null && (
+
+          {!isMobileView && (
+            <Row gutter={[24, 24]}>
+              <Col xs={24} md={12}>
                 <HistoryCard
                   compareMode={compareMode}
-                  comparisonData={liveStatement}
-                  status={liveStatement?.status}
-                  currentVersion={currentVersion}
+                  comparisonData={s1}
+                  status={itemsStatus[s1?.id]}
+                  s1={true}
                 />
+              </Col>
+              <Col xs={24} md={12}>
+                <HistoryCard
+                  compareMode={compareMode}
+                  comparisonData={s2}
+                  status={itemsStatus[s2?.id]}
+                />
+              </Col>
+              {liveStatement !== null && (
+                <Col xs={24} md={24}>
+                  <HistoryCard
+                    compareMode={compareMode}
+                    comparisonData={liveStatement}
+                    status={liveStatement?.status}
+                    currentVersion={currentVersion}
+                  />
+                </Col>
               )}
-            </Col>
-          </Row>
-          <ComparisonMobileTabs />
+            </Row>
+          )}
+
+          {isMobileView && (
+            <Tabs
+              defaultActiveKey="1"
+              centered
+              className="comparision-mobile-tabs live-tab"
+            >
+              <Tabs.TabPane
+                className="comparison-tab-content"
+                tab={
+                  <>
+                    <p>{convertToDate(s1?.submit_time)}</p>
+                    <span>{convertToTime(s1?.submit_time)}</span>
+                  </>
+                }
+                key="1"
+              >
+                <Col xs={24} md={12}>
+                  <HistoryCard
+                    compareMode={compareMode}
+                    comparisonData={s1}
+                    status={itemsStatus[s1?.id]}
+                    s1={true}
+                  />
+                </Col>
+              </Tabs.TabPane>
+
+              <Tabs.TabPane
+                className="comparison-tab-content"
+                tab={
+                  <>
+                    <p>{convertToDate(s2?.submit_time)}</p>
+                    <span>{convertToTime(s2?.submit_time)}</span>
+                  </>
+                }
+                key="2"
+              >
+                <Col xs={24} md={12}>
+                  <HistoryCard
+                    compareMode={compareMode}
+                    comparisonData={s2}
+                    status={itemsStatus[s2?.id]}
+                  />
+                </Col>
+              </Tabs.TabPane>
+            </Tabs>
+          )}
         </div>
       )}
     </>
