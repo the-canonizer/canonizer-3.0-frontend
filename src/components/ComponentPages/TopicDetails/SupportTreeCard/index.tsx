08a9f2c7c4ff85de82d4dae65d3058a6025144a6
wahaj.hafeez <wahaj.hafeez@codedistrict.com>
2024-08-20 11:26:13 +0500

    sign petition btn styling issue fixed

--- src/components/ComponentPages/TopicDetails/SupportTreeCard/index.tsx
+++ src/components/ComponentPages/TopicDetails/SupportTreeCard/index.tsx
@@ -765,12 +765,12 @@ const SupportTreeCard = ({
           <Popover content={renderPopupMsg()}>
             <Button
               size="large"
-              className="flex items-center justify-center h-[44px] border-[#4EB966] hover:!text-canBlack hover:!border-[#4EB966] hover:!bg-[#4EB9661A] bg-[#4EB9661A] rounded-lg font-medium text-sm gap-2"
+              className="flex items-center justify-center h-[44px] border-[#4EB966] hover:!text-canBlack hover:!border-[#4EB966] hover:!bg-[#4EB9661A] bg-[#4EB9661A] rounded-lg font-medium text-sm"
               block
               disabled={disableSignPetition()}
               onClick={() => signPetitionHandler()}
             >
-              Sign Petition<i className="icon-user-plus"></i>
+              Sign Petition<i className="icon-user-plus ml-2"></i>
             </Button>
           </Popover>
         </div>
