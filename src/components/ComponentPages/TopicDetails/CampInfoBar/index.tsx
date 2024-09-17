e476ea48f5d91a721d229faf307e3d43125fffe0
vikasrana <vikas.rana@talentelgia.in>
2024-09-17 17:15:27 +0530

    revert content
    revert content

--- src/components/ComponentPages/TopicDetails/CampInfoBar/index.tsx
+++ src/components/ComponentPages/TopicDetails/CampInfoBar/index.tsx
@@ -1207,7 +1207,7 @@ const TimelineInfoBar = ({
                   </div>
                 ) : null}
 
-                {!isHtmlContent && !isHistoryPage && !compareMode && (
+                {!isHtmlContent && !isHistoryPage && !compareMode && campRecord?.is_archive == 0 && (
                   <SecondaryButton
                     className="hidden px-8 py-2.5 lg:flex items-center text-sm gap-1"
                     size="large"
