f16147b67560e2ba55b9217d9c51c9b156275fab
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-09-26 18:19:29 +0500

    fixed issue 1199

--- src/components/ComponentPages/CreateNewCamp/updateCamp.tsx
+++ src/components/ComponentPages/CreateNewCamp/updateCamp.tsx
@@ -63,6 +63,7 @@ const CreateNewCamp = () => {
   const [editStatementData, setEditStatementData] = useState({ data: null });
   const [isSubmitReq, setIsSubmitReq] = useState(false);
   const [isTopicLoading, setIsopicLoading] = useState(false);
+  const [editCampData,setEditCampData] = useState(null);
 
   const update = router?.query?.camp?.at(0)?.split("-")[1] == "update";
 
@@ -177,7 +178,8 @@ const CreateNewCamp = () => {
 
       const res = await getEditCampApi(getDataPayload),
         resData = res?.data;
-
+        setEditCampData(resData)
+        
       fetchCampNickNameList();
 
       if (res && res?.status_code == 404) {
@@ -319,6 +321,17 @@ const CreateNewCamp = () => {
     }
     setIsLoading(false);
   };
+
+  const getCampLeaderData = () => {
+    let camp_leader_nick_id = editCampData?.camp?.camp_leader_nick_id;
+    let camp_leader_nick_name = "";
+    if (camp_leader_nick_id){
+      camp_leader_nick_name =  editCampData?.eligible_camp_leaders?.find((CL) => CL?.camp_leader === true)?.nick_name
+    }else{
+      camp_leader_nick_name = "No Camp Leader"
+    } 
+    return camp_leader_nick_name
+  }
 
   const isSimilarAvaiable = () => {
     const namesList = existingCamps?.map((cmp) =>
@@ -576,6 +589,7 @@ const CreateNewCamp = () => {
                 onCampNameBlur={onCampNameBlur}
                 values={values}
                 campLeaderData={campLeaderData}
+                getCampLeaderData={getCampLeaderData}
               />
             </Col>
             <Col lg={12}>
