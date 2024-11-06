401a67b34d693770e3dc599701c9edef560627ca
vikasrana <vikas.rana@talentelgia.in>
2024-11-06 16:23:43 +0530

    Ticket #2299
    Refine Filter issue

--- src/components/ComponentPages/LatestFilter/index.tsx
+++ src/components/ComponentPages/LatestFilter/index.tsx
@@ -138,6 +138,7 @@ const LatestFilter = () => {
     dispatch(setViewThisVersion(false));
     dispatch(
       setFilterCanonizedTopics({
+        asofdate: Date.now() / 1000,
         asof: "default",
       })
     );
@@ -151,6 +152,7 @@ const LatestFilter = () => {
       viewThisVersion
     );
   };
+ 
   const filterscore = () => {
     dispatch(setViewThisVersion(false));
     dispatch(
@@ -354,7 +356,7 @@ const LatestFilter = () => {
               data-testid="close_icon_as_of_date"
             /> */}
             <div
-              onClick={filterForAsofDate}
+              onClick={()=>{filterForAsofDate()}}
               data-testid="close_icon_as_of_date"
             >
               <Image
