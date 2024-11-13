f611bf8fb8e3364b71679bac4ed0f5a1700ba68f
vikasrana <vikas.rana@talentelgia.in>
2024-11-13 17:14:13 +0530

    Ticket #1182 #1309
    Nickname must not accept only special characters etc.
    As of date (Custom Date) and View This version : Color must be blue on topic detail page

--- src/components/ComponentPages/TopicDetails/CampStatementCard/index.tsx
+++ src/components/ComponentPages/TopicDetails/CampStatementCard/index.tsx
@@ -174,10 +174,11 @@ const CampStatementCard = ({ loadingIndicator }) => {
       }}
       className={`border-0 h-100 bg-white [&_.ant-card-body]:p-0 [&_.ant-card-body]:lg:p-[24px] [&_.ant-card-body]:flex overflow-hidden lg:bg-canGray mb-8 lg:mb-14 border-t-8 
         ${
+          router?.query?.viewversion == "1" ? "!border-canOrange" :
           router?.query?.asof == "review"
             ? "!border-canOrange"
             : router?.query?.asof == "bydate"
-            ? "border-[#4786CB]"
+            ? "border-[#4786CB]" 
             : "!border-canGreen"
         } 
           h-[400px] xl:h-[600px] statementCardBody`}
