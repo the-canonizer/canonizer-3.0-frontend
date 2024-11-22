c0653945812980f8abcf55f71c86ede6890a0878
Rahul Kumar Singh <rahul.singh@iffort.com>
2024-11-22 16:36:01 +0530

    ticket #1289 is ficed.

--- src/hooks/isUserAuthenticated.ts
+++ src/hooks/isUserAuthenticated.ts
@@ -3,18 +3,22 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store/index";
 
 const useAuthentication = () => {
-  const { authenticated, id } = useSelector((state: RootState) => ({
+  const { authenticated, id, logOutType } = useSelector((state: RootState) => ({
     authenticated: state.auth?.authenticated,
     id: state.auth?.loggedInUser?.id,
+    logOutType: state.auth?.logOutType,
   }));
+
   const [isUserAuthenticated, setIsUserAuthenticated] = useState(authenticated);
+
   const [userID, setuserID] = useState(id);
+
   useEffect(() => {
     setIsUserAuthenticated(authenticated);
     setuserID(id);
   }, [authenticated, id]);
 
-  return { isUserAuthenticated, userID };
+  return { isUserAuthenticated, userID, logOutType };
 };
 
 export default useAuthentication;
