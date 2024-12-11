f8481cce37c91fd80334b9c2a58c2a7a5385b813
Rahul Kumar Singh <rahul.singh@iffort.com>
2024-12-11 11:43:51 +0530

    ids.

--- src/components/ComponentPages/SettingsUI/index.tsx
+++ src/components/ComponentPages/SettingsUI/index.tsx
@@ -48,7 +48,8 @@ const SettingsUI = () => {
   const [selectedTab, setSelectedTab] = useState("Direct_Supported_Camps");
   const [getDataFromUserProfile, setGetDataFromUserProfile] = useState(null);
   const [openKeys, setOpenKeys] = useState([]);
-  const [selectedValueFromSelectTag, setSelectedValueFromSelectTag] = useState("");
+  const [selectedValueFromSelectTag, setSelectedValueFromSelectTag] =
+    useState("");
 
   const onTabChange = (key) => {
     setActiveTabKey(key);
@@ -203,6 +204,7 @@ const SettingsUI = () => {
       key: "profile_info",
       itemIcon: (
         <Image
+          id="menu_item_profile_info_icon"
           src="/images/nickname-user-icon.svg"
           width={14}
           height={14}
@@ -211,7 +213,7 @@ const SettingsUI = () => {
       ),
       label: (
         <Link href="/settings?tab=profile_info">
-          <a>Personal Info</a>
+          <a id="menu_item_profile_info_link">Personal Info</a>
         </Link>
       ),
       className: `listItem ${getMatchQuery("profile_info")}`,
@@ -220,6 +222,7 @@ const SettingsUI = () => {
       key: "nick_name",
       itemIcon: (
         <Image
+          id="menu_item_nick_name_icon"
           src="/images/nickname-user-icon.svg"
           width={14}
           height={14}
@@ -228,7 +231,7 @@ const SettingsUI = () => {
       ),
       label: (
         <Link href="/settings?tab=nick_name">
-          <a>Nicknames</a>
+          <a id="menu_item_nick_name_link">Nicknames</a>
         </Link>
       ),
       className: `listItem ${getMatchQuery("nick_name")}`,
@@ -237,6 +240,7 @@ const SettingsUI = () => {
       key: "user_preferences",
       itemIcon: (
         <Image
+          id="menu_item_user_preferences_icon"
           src="/images/preference-icon.svg"
           width={14}
           height={14}
@@ -245,7 +249,7 @@ const SettingsUI = () => {
       ),
       label: (
         <Link href="/settings?tab=user_preferences">
-          <a>Preferences</a>
+          <a id="menu_item_user_preferences_link">Preferences</a>
         </Link>
       ),
       className: `listItem ${getMatchQuery("user_preferences")}`,
@@ -253,9 +257,17 @@ const SettingsUI = () => {
     {
       key: "supported_camps",
       label: (
-        <span className="flex justify-start gap-1">
+        <span
+          id="menu_item_supported_camps_label"
+          className="flex justify-start gap-1"
+        >
           Supported Camps
-          <Image src="/images/flagicon.svg" width={24} height={24} />
+          <Image
+            id="menu_item_supported_camps_icon"
+            src="/images/flagicon.svg"
+            width={24}
+            height={24}
+          />
         </span>
       ),
       children: [
@@ -263,7 +275,9 @@ const SettingsUI = () => {
           key: "direct_supported_camps",
           label: (
             <Link href="/settings?tab=direct_supported_camps">
-              <a>Direct Supported Camps</a>
+              <a id="menu_item_direct_supported_camps_link">
+                Direct Supported Camps
+              </a>
             </Link>
           ),
           className: `subItem ${getMatchQuery("direct_supported_camps")}`,
@@ -272,7 +286,9 @@ const SettingsUI = () => {
           key: "delegate_supported_camp",
           label: (
             <Link href="/settings?tab=delegate_supported_camp">
-              <a>Delegated Supported Camps</a>
+              <a id="menu_item_delegate_supported_camp_link">
+                Delegated Supported Camps
+              </a>
             </Link>
           ),
           className: `subItem ${getMatchQuery("delegate_supported_camp")}`,
@@ -284,6 +300,7 @@ const SettingsUI = () => {
       key: "subscriptions",
       itemIcon: (
         <Image
+          id="menu_item_subscriptions_icon"
           src="/images/subscription-icon.svg"
           width={14}
           height={14}
@@ -292,7 +309,7 @@ const SettingsUI = () => {
       ),
       label: (
         <Link href="/settings?tab=subscriptions">
-          <a>My Subscriptions</a>
+          <a id="menu_item_subscriptions_link">My Subscriptions</a>
         </Link>
       ),
       className: `listItem ${getMatchQuery("subscriptions")}`,
@@ -300,9 +317,18 @@ const SettingsUI = () => {
     {
       key: "account_settings",
       label: (
-        <span className="flex justify-start gap-1">
+        <span
+          id="menu_item_account_settings_label"
+          className="flex justify-start gap-1"
+        >
           Account Settings
-          <Image src="/images/setting-icon.svg" width={24} height={24} alt="" />
+          <Image
+            id="menu_item_account_settings_icon"
+            src="/images/setting-icon.svg"
+            width={24}
+            height={24}
+            alt=""
+          />
         </span>
       ),
       children: [
@@ -310,7 +336,7 @@ const SettingsUI = () => {
           key: "social_oauth_verification",
           label: (
             <Link href="/settings?tab=social_oauth_verification">
-              <a>Social Auth</a>
+              <a id="menu_item_social_oauth_verification_link">Social Auth</a>
             </Link>
           ),
           className: `subItem ${getMatchQuery("social_oauth_verification")}`,
@@ -319,7 +345,7 @@ const SettingsUI = () => {
           key: "change_password",
           label: (
             <Link href="/settings?tab=change_password">
-              <a>Password</a>
+              <a id="menu_item_change_password_link">Password</a>
             </Link>
           ),
           className: `subItem ${getMatchQuery("change_password")}`,
@@ -350,37 +376,37 @@ const SettingsUI = () => {
       getUesrPofileData();
     }
   }, []);
-console.log(router,"rout")
-useEffect(() => {
-  if (router.query.tab) {
-    const tab = router.query.tab as string;
-    switch (tab) {
-      case "nick_name":
-        setSelectedValueFromSelectTag("Nicknames");
-        break;
-      case "profile_info":
-        setSelectedValueFromSelectTag("Personal Info");
-        break;
-      case "user_preferences":
-        setSelectedValueFromSelectTag("Preferences");
-        break;
-      case "direct_supported_camps":
-        setSelectedValueFromSelectTag("Supported Camps");
-        break;
+  console.log(router, "rout");
+  useEffect(() => {
+    if (router.query.tab) {
+      const tab = router.query.tab as string;
+      switch (tab) {
+        case "nick_name":
+          setSelectedValueFromSelectTag("Nicknames");
+          break;
+        case "profile_info":
+          setSelectedValueFromSelectTag("Personal Info");
+          break;
+        case "user_preferences":
+          setSelectedValueFromSelectTag("Preferences");
+          break;
+        case "direct_supported_camps":
+          setSelectedValueFromSelectTag("Supported Camps");
+          break;
         case "delegate_supported_camp":
-        setSelectedValueFromSelectTag("Supported Camps");
-        break;
-      case "social_oauth_verification":
-        setSelectedValueFromSelectTag("Social Auth");
-        break;
-      case "change_password":
-        setSelectedValueFromSelectTag("Change Password");
-        break;
-      default:
-        setSelectedValueFromSelectTag(undefined);
+          setSelectedValueFromSelectTag("Supported Camps");
+          break;
+        case "social_oauth_verification":
+          setSelectedValueFromSelectTag("Social Auth");
+          break;
+        case "change_password":
+          setSelectedValueFromSelectTag("Change Password");
+          break;
+        default:
+          setSelectedValueFromSelectTag(undefined);
+      }
     }
-  }
-}, [router.query.tab]);
+  }, [router.query.tab]);
   return (
     <div
       className="pageContentWrap flex lg:flex-row flex-col gap-10"
@@ -500,7 +526,10 @@ useEffect(() => {
                           {
                             value: "Personal Info",
                             label: (
-                              <span id="setting_section_select_tag_persnol_info" className="span_tagpersonal_info">
+                              <span
+                                id="setting_section_select_tag_persnol_info"
+                                className="span_tagpersonal_info"
+                              >
                                 <Link
                                   href="/settings?tab=profile_info"
                                   className="[&_.ant-menu-item-selected]:!text-canBlue"
@@ -528,7 +557,7 @@ useEffect(() => {
                           {
                             value: "Nicknames",
                             label: (
-                              <span id="setting_section_select_tag_nickname" >
+                              <span id="setting_section_select_tag_nickname">
                                 <Link
                                   href="/settings?tab=nick_name"
                                   className="[&_.ant-menu-item]:!rounded-lg"
@@ -587,7 +616,10 @@ useEffect(() => {
                           {
                             value: "Supported Camps",
                             label: (
-                              <span id="setting_section_select_tag_supported_camps"  className="span_tagpersonal_info">
+                              <span
+                                id="setting_section_select_tag_supported_camps"
+                                className="span_tagpersonal_info"
+                              >
                                 <a
                                   className="flex items-center gap-3"
                                   id="setting_section_select_tag_supported_camps_all_link"
