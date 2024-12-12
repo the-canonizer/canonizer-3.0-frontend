fcd995f5cbf44e964c84ac18428fff0e59c22d92
wahaj.hafeez <wahaj.hafeez@codedistrict.com>
2024-12-12 15:57:51 +0500

    show and hide score on topic detail page

--- src/components/ComponentPages/TopicDetails/CampTree/index.tsx
+++ src/components/ComponentPages/TopicDetails/CampTree/index.tsx
@@ -12,6 +12,7 @@ import { RootState } from "src/store";
 import { setCurrentCamp } from "src/store/slices/filtersSlice";
 import { replaceSpecialCharacters } from "src/utils/generalUtility";
 import { DownOutlined } from "@ant-design/icons";
+import ScoreTag from "components/ComponentPages/Home/TrandingTopic/scoreTag";
 
 const { TreeNode } = Tree;
 
@@ -506,7 +507,7 @@ const CampTree = ({
                           data[item]
                         )}
                     </span>
-                    <span className="bg-canOrange px-[0.30rem] rounded-md flex items-center gap-1">
+                    {/* <span className="bg-canOrange px-[0.30rem] rounded-md flex items-center gap-1">
                       <Image
                         src="/images/hand-icon.svg"
                         alt="svg"
@@ -518,7 +519,14 @@ const CampTree = ({
                           ? data[item].full_score?.toFixed(2)
                           : data[item].score?.toFixed(2)}
                       </span>
-                    </span>
+                    </span> */}
+                     <ScoreTag
+                        topic_score={
+                          is_checked
+                          ? data[item]?.full_score
+                          : data[item]?.score
+                        }
+                      />
                   </div>
                 </div>
               }
