a05e0a5db908f4d2c7fd4f7ee05ab4d4d3007963
Rahul Kumar Singh <rahul.singh@iffort.com>
2024-12-13 11:11:10 +0530

    #1249 : tree connectivity issue is inn rpogress.

--- src/components/ComponentPages/TopicDetails/CampTree/index.tsx
+++ src/components/ComponentPages/TopicDetails/CampTree/index.tsx
@@ -274,20 +274,25 @@ const CampTree = ({
     ) : null;
   };
 
-  const isOnlyOneChild = () => {
-    const treeData = tree?.at(0);
-    if (!treeData) return false;
-
-    const keys = Object.keys(treeData)
-      .map((key) => [Number(key), treeData[key]])
-      .sort((a, b) => b[1].score - a[1].score);
-
-    const keyName = keys?.at(0)?.[0];
-    if (!keyName) return false;
-
-    const child = treeData[keyName]?.children;
-    return child?.length === 0;
+  const isOnlyOneChild = (node) => {
+    return node?.children && Object.keys(node.children).length === 1;
   };
+
+  const isFirstChild = (data, item) => {
+    const keys = Object.keys(data);
+    return keys[0] === item.toString();
+  };
+
+  const isLastChild = (data, item) => {
+    const keys = Object.keys(data);
+    return keys[keys.length - 1] === item.toString();
+  };
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     document.querySelectorAll
+  //   }, 300);
+  // }, [uniqueKeys]);
 
   const renderTreeNodes = (
     data: any,
@@ -325,44 +330,24 @@ const CampTree = ({
         }
       }
 
-      const isLastChild = (data, item) => {
-        const keys = Object.keys(data);
-        return keys[keys.length - 1] === item.toString();
-      };
+      const firstChild = isFirstChild(data, itemWithData[0]);
+      const lastChild = isLastChild(data, itemWithData[0]);
+      const onlyOneChild = isOnlyOneChild(item);
 
-      const isLastParent = (data, item) => {
-        const parentKeys = Object.keys(data);
-        return parentKeys[parentKeys.length - 1] === item.toString();
-      };
-
-      const isLast = isLastChild(data, item) && isLastParent(data, item);
-
-      const isFirstItem = (data, item) => {
-        const keys = Object.keys(data);
-        return keys[0] === item.toString();
-      };
-
-      const isFirstParent = (data, item) => {
-        const parentKeys = Object.keys(data);
-        return parentKeys[0] === item.toString();
-      };
-
-      const isFirst = isFirstItem(data, item) && isFirstParent(data, item);
-
-      const haveOnlyOneChild = isOnlyOneChild();
+      const dynamicClasses = [
+        firstChild ? "first-node-class" : "",
+        lastChild ? "last-node-class" : "",
+        onlyOneChild ? "only-one-child" : "",
+      ]
+        .filter(Boolean)
+        .join(" ");
 
       if (data[item].children) {
         if (data[item].score >= scoreFilter) {
           return data[item].is_archive == 0 ||
             (data[item].is_archive != 0 && is_camp_archive_checked == true) ? (
             <TreeNode
-              className={`[&_.ant-tree-switcher]:!flex [&_.ant-tree-switcher]:!items-center [&_.ant-tree-node-content-wrapper]:hover:!bg-transparent [&_.ant-tree-switcher.ant-tree-switcher-noop>span]:!hidden [&_.ant-tree-node-content-wrapper]:py-1 ${
-                isLastChild ? "last-child-node-class" : ""
-              } ${isLastParent ? "last-parent-node-class" : ""} ${
-                isLast ? "last-node-class" : ""
-              } ${isFirst ? "first-node-class" : ""} ${
-                haveOnlyOneChild ? "only-one-child" : ""
-              }`}
+              className={`[&_.ant-tree-switcher]:!flex [&_.ant-tree-switcher]:!items-center [&_.ant-tree-node-content-wrapper]:hover:!bg-transparent [&_.ant-tree-switcher.ant-tree-switcher-noop>span]:!hidden [&_.ant-tree-node-content-wrapper]:py-1 ${dynamicClasses}`}
               switcherIcon={({ expanded }) => {
                 return data[item].camp_id ===
                   +(router?.query?.camp?.at(1)?.split("-")?.at(0) ?? 1) &&
@@ -617,9 +602,12 @@ const CampTree = ({
   };
 
   let treeContent;
-  
+
   if (tree?.at(0)) {
-    if ((showTree && tree?.at(0)["1"]?.title != "" && defaultExpandKeys) || isForumPage) {
+    if (
+      (showTree && tree?.at(0)["1"]?.title != "" && defaultExpandKeys) ||
+      isForumPage
+    ) {
       treeContent = (
         <Tree
           showLine
@@ -638,7 +626,7 @@ const CampTree = ({
   } else {
     treeContent = <p data-testid="camp-tree">No Camp Tree Found</p>;
   }
-  
+
   return treeContent;
 };
 
