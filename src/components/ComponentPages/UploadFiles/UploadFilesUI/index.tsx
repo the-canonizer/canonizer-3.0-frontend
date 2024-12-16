4c6d9a7d22a86f6fa2e18c5fd7b849c210a5ee18
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-12-16 15:47:19 +0500

    fixed issue 1297

--- src/components/ComponentPages/UploadFiles/UploadFilesUI/index.tsx
+++ src/components/ComponentPages/UploadFiles/UploadFilesUI/index.tsx
@@ -1104,7 +1104,11 @@ const UploadFileUI = ({
     const dateparams =
       dateString == null ? "" : Math.floor(dateString?.valueOf() / 1000);
     const date =
-      dateString == null ? "" : queryString ? epochTimeInSeconds : dateparams;
+      dateString == null
+        ? ""
+        : dateString != null
+        ? dateparams
+        : epochTimeInSeconds;
     let response = await globalSearchUploadFiles(
       queryParams({ query: queryString, date })
     );
@@ -1246,7 +1250,7 @@ const UploadFileUI = ({
                               // uploadStatus == true
                               //   ? setDatePick("")
                               //   :
-                              setDatePick(date ? date.toLocaleString() : "");
+                              setDatePick(date && date);
                               getGlobalSearchUploadFile(search, date);
                             }}
                             value={datePick && moment(new Date(datePick))}
