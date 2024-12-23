40ada2241c6ab45dab3ae1ade58e19bc82fc4d20
wahaj.hafeez <wahaj.hafeez@codedistrict.com>
2024-12-23 15:59:46 +0500

    add current user in server side api call

--- src/components/ComponentPages/Login/index.tsx
+++ src/components/ComponentPages/Login/index.tsx
@@ -99,6 +99,14 @@ const Login = () => {
     }
 
     if (res && res.status_code === 200) {
+      document.cookie = "current_user=" +
+      username +
+      "; expires=Thu, 15 Jul 2030 00:00:00 UTC; path=/";
+
+      document.cookie = "isUserAuthenticated=" +
+      true +
+      "; expires=Thu, 15 Jul 2030 00:00:00 UTC; path=/";
+
       dispatch(
         setFilterCanonizedTopics({
           algorithm: res?.data?.user?.default_algo,
