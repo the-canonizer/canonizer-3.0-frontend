6522673484a74a218f5ec8ac964a210f938d573f
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2025-01-06 13:07:23 +0500

    inprogress 1605

--- src/components/ComponentPages/UploadFiles/UploadFilesUI/index.tsx
+++ src/components/ComponentPages/UploadFiles/UploadFilesUI/index.tsx
@@ -1231,6 +1231,11 @@ const UploadFileUI = ({
                         onClick={handleGoBack}
                       >
                         File(s) Uploaded
+                        <h3>
+                          <span className={`${styles.span} ml-1.5`}>
+                            {messages.labels.maxSize}
+                          </span>
+                        </h3>
                       </Button>
                       {/* <h3>
                         {messages.labels.uploadFiles}
