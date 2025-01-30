c4f2cc26dcbda5e186840cba7f6f1a539c2265ab
vikasrana <vikas.rana@talentelgia.in>
2025-01-30 16:53:43 +0530

    Ticket #1684
    Build issue

--- src/components/ComponentPages/searchCanonizer/campStatement.tsx
+++ src/components/ComponentPages/searchCanonizer/campStatement.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import SearchSideBar from "../../common/SearchSideBar";
 import styles from "./search.module.scss";
-// import AdvanceFilter from "../../common/AdvanceSearchFilter";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "src/store";
 import moment from "moment";
--- src/components/ComponentPages/searchCanonizer/topic.tsx
+++ src/components/ComponentPages/searchCanonizer/topic.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import SearchSideBar from "../../common/SearchSideBar";
 import styles from "./search.module.scss";
-// import AdvanceFilter from "../../common/AdvanceSearchFilter";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "src/store";
