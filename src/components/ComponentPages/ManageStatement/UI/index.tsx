1f07591c2042160880898eb3c935189679d6fd99
vikasrana <vikas.rana@talentelgia.in>
2025-02-04 13:42:48 +0530

    Ticket #1697
    Camp statement page - Need to add a note when uploading any image in the editor

--- src/components/ComponentPages/ManageStatement/UI/index.tsx
+++ src/components/ComponentPages/ManageStatement/UI/index.tsx
@@ -283,6 +283,12 @@ function ManageStatementUI({
               />
             </Col>
             <Col xs={24} xl={24} id="statement-col">
+              <p
+                className="mt-1 mb-5 text-sm font-normal text-canBlack opacity-80"
+                id="nickanme_note"
+              >
+                Note: The maximum allowed file size is 5 MB.
+              </p>
               <Form.Item
                 className="mb-2 editorContent [&_.ant-form-item-label>label]:w-full"
                 name="statement"
