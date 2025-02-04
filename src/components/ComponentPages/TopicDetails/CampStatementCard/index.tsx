9bb36678da52238dcca6f6ec4a0316fb06601c2e
vikasrana <vikas.rana@talentelgia.in>
2025-02-04 16:48:13 +0530

    Ticket #1680
    Camp statement in review filter should show nothing if there is no statement ,camp or topic in review

--- src/components/ComponentPages/TopicDetails/CampStatementCard/index.tsx
+++ src/components/ComponentPages/TopicDetails/CampStatementCard/index.tsx
@@ -27,12 +27,13 @@ import SectionHeading from "components/ComponentPages/Home/FeaturedTopic/section
 const CampStatementCard = ({ loadingIndicator }) => {
   const router = useRouter();
 
-  const { campRecord, campStatement, tree, haveStatementPreview } = useSelector(
+  const { campRecord, campStatement, tree, haveStatementPreview,asof } = useSelector(
     (state: RootState) => ({
       campStatement: state?.topicDetails?.campStatement,
       campRecord: state?.topicDetails?.currentCampRecord,
       tree: state?.topicDetails?.tree && state?.topicDetails?.tree[0],
       haveStatementPreview: state?.topic?.haveStatementPreview,
+      asof: state?.filters?.filterObject?.asof,
     })
   );
   const [fullScreen, setFullScreen] = useState(false);
@@ -297,12 +298,14 @@ const CampStatementCard = ({ loadingIndicator }) => {
           <div
             className={`${styles.campStatement} text-canBlack opacity-80 text-sm font-normal leading-6 [&_a]:!text-canBlue [&_a]:hover:!text-canHoverBlue`}
           >
-            {campStatement?.length && campStatement[0]?.parsed_value ? (
+            {campStatement?.length && campStatement[0]?.parsed_value && asof !== "review" ? (
               <div
                 dangerouslySetInnerHTML={{
                   __html: `<div class="ck-content editorContent">${campStatement[0]?.parsed_value}</div>`,
                 }}
               />
+            ) :campStatement?.length && campStatement[0]?.parsed_value && asof == "review"&& campStatement?.at(0)?.in_review_changes == 0?(
+              "There is no statement in review"
             ) : (
               <span className="text-sm">
                 {isDraftShow()
