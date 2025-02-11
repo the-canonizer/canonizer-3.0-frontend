b9b9533b5f6d37206a2bd491e095c680d4ca4bd0
wahaj.hafeez <wahaj.hafeez@codedistrict.com>
2025-02-11 16:03:26 +0500

    added handler func for disabling objChange and edit or archive btns

--- src/components/ComponentPages/HistoryCard/historyCard.tsx
+++ src/components/ComponentPages/HistoryCard/historyCard.tsx
@@ -328,6 +328,32 @@ function HistoryCard({
     setIsModalOpen(true);
   }
 
+  const disableObjectChangesBtn = () => {
+    return historyOf == "camp"
+    ? !campStatement?.ifICanAgreeAndObject
+    : false
+  }
+
+  const disableEditOrUnArchiveBtn = () => {
+    return unarchiveChangeSubmitted ||
+    (campHistoryItems &&
+      campHistoryItems[0]?.status == "in_review" &&
+      !commited &&
+      !!campHistoryItems[0]?.grace_period) ||
+    (campHistoryItems?.at(0)?.status == "live" &&
+      campHistoryItems?.at(0)?.is_archive == 1 &&
+      campStatement.status == "old") ||
+    (parentArchived == 1 && directarchived == 0) ||
+    (parentArchived == 1 &&
+      directarchived == 1 &&
+      historyOf == "topic") ||
+    (campHistoryItems?.at(0)?.is_archive == 1 &&
+      campHistoryItems?.at(0)?.status == "live" &&
+      campStatement.status == "objected")
+      ? true
+      : false
+  }
+
   return (
     <div
       id="history-card-container"
@@ -601,23 +627,7 @@ function HistoryCard({
                     : submitUpdateRedirect(historyOf);
                 }}
                 disabled={
-                  unarchiveChangeSubmitted ||
-                  (campHistoryItems &&
-                    campHistoryItems[0]?.status == "in_review" &&
-                    !commited &&
-                    !!campHistoryItems[0]?.grace_period) ||
-                  (campHistoryItems?.at(0)?.status == "live" &&
-                    campHistoryItems?.at(0)?.is_archive == 1 &&
-                    campStatement.status == "old") ||
-                  (parentArchived == 1 && directarchived == 0) ||
-                  (parentArchived == 1 &&
-                    directarchived == 1 &&
-                    historyOf == "topic") ||
-                  (campHistoryItems?.at(0)?.is_archive == 1 &&
-                    campHistoryItems?.at(0)?.status == "live" &&
-                    campStatement.status == "objected")
-                    ? true
-                    : false
+                    disableEditOrUnArchiveBtn()
                 }
               >
                 {campStatement?.is_archive == 1 &&
@@ -646,11 +656,7 @@ function HistoryCard({
                   >
                     <Button
                       size="large"
-                      disabled={
-                        historyOf == "camp"
-                          ? !campStatement?.ifICanAgreeAndObject
-                          : false
-                      }
+                      disabled={disableObjectChangesBtn()}
                       id={`object-change-${campStatement?.id}`}
                       className="flex items-center bg-canRed_Opacity10 border-canRed hover:border-canRed hover:text-canRed focus:text-canRed focus:border-canRed justify-center text-sm rounded-xl gap-3.5 leading-none w-100 font-medium"
                       onClick={() => objectionHandler()}
