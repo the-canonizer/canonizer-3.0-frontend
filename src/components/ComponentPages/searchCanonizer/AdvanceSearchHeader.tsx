2c72c4ca2b779e248d1f18a790eb36eacc4caaef
DevAlchemy <wahaj.hafeez@codedistrict.com>
2025-02-13 16:26:54 +0500

    fix the build issue

--- src/components/ComponentPages/searchCanonizer/AdvanceSearchHeader.tsx
+++ src/components/ComponentPages/searchCanonizer/AdvanceSearchHeader.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import AdvanceFilter from "components/common/AdvanceSearchFilter";
+import React from "react";
 
 interface AdvanceSearchHeaderProps {
   sectionId: string;
