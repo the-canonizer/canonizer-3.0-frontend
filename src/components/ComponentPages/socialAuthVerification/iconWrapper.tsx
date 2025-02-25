6833db3082f60786a535eae22f9049c3d21b4353
Rahul Kumar Singh <rahul.singh@iffort.com>
2025-02-25 16:47:00 +0530

    ITR11:#1685:- linkedin account email id correction.

--- src/components/ComponentPages/socialAuthVerification/iconWrapper.tsx
+++ src/components/ComponentPages/socialAuthVerification/iconWrapper.tsx
@@ -17,6 +17,13 @@ function IconWrapper({
     })
   );
 
+  //   <Text strong className={`${styles.name}`}>
+  //   {socialLinks[provider + "_name"]}
+  // </Text>
+  // <Text className={`${styles.email}`}>
+  //   {socialLinks[provider + "_email"]}
+  // </Text>
+
   return (
     <div className="border border-canGrey2 rounded-lg flex justify-center bg-canGray">
       <div className="w-full ">
@@ -24,20 +31,20 @@ function IconWrapper({
           <div className="flex items-center"> {icon}</div>
           <div className="lg:hidden flex flex-col justify-start items-start">
             <h3 className="mb-1 text-center text-base font-medium text-canBlack">
-              {globalUserProfileData}
+              {socialLinks[provider + "_name"] ?? globalUserProfileData}
             </h3>
             <p className=" text-center text-sm font-normal text-canBlack">
-              {globalUserProfileDataEmail}
+              {socialLinks[provider + "_email"] ?? globalUserProfileDataEmail}
             </p>
           </div>
         </div>
         <div className="px-5 py-5  w-full flex flex-col justify-center items-center rounded-bl-lg rounded-br-lg ">
           <div className="lg:flex hidden flex-col">
             <h3 className="mb-1 text-center text-base font-medium text-canBlack">
-              {globalUserProfileData}
+              {socialLinks[provider + "_name"] ?? globalUserProfileData}
             </h3>
             <p className="mb-5 text-center text-sm font-normal text-canBlack">
-              {globalUserProfileDataEmail}
+              {socialLinks[provider + "_email"] ?? globalUserProfileDataEmail}
             </p>
           </div>
 
