73fdbdfc1945f1b14ccad21bba0403091ad11868
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2025-03-26 12:51:27 +0530

    Revert "add https agent in the network call"

--- src/network/networkCall.ts
+++ src/network/networkCall.ts
@@ -12,9 +12,6 @@ import { createNewToken, logout } from "./api/userApi";
 import { store } from "../store";
 import { updateStatus } from "../store/slices/uiSlice";
 import { setLoadingAction } from "src/store/slices/loading";
-import https from "https"; // Import https module
-
-
 
 export default class NetworkCall {
   static counter = 1;
@@ -119,15 +116,8 @@ export default class NetworkCall {
     throw new Error("Method not implemented.");
   }
 }
-
-// Create an HTTPS Agent that disables SSL verification
-const agent = new https.Agent({
-  rejectUnauthorized: false, // Disable SSL certificate verification
-});
-
 NetworkCall.axios = axios.create({
   baseURL: K.Network.URL.BaseAPI,
   timeout: +K.Network.URL.Timeout,
   headers: {},
-  httpsAgent: agent, // Attach the custom HTTPS Agent
 });
